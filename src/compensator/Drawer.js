f04177ee1db5fa8ba660bcc6792ba32a365d7ab0
CAScgutierrez <cgutierrez@humboldt.org.co>
2018-07-31 10:08:04 -0500

    tabla donde

--- src/compensator/Drawer.js
+++ src/compensator/Drawer.js
@@ -186,7 +186,255 @@ console.log("biomaColor: "+biomaColor);
             </Tabs>
           </AppBar>
           {value === 0 && <TabContainer>
-            Gráfico ¿Qué y cuánto?
+            <div className="total">
+            <h3>Total a compensar</h3>
+            <h4>{Number(this.state.totalACompensar).toFixed(2)}</h4>
+            </div>
+            <table className="graphcard">
+              <tr className="row1table">
+                <th>BIOMA IAVH</th>
+                <th>F.C.</th>
+                <th>NAT.</th>
+                <th>SEC.</th>
+                <th>TRANS.</th>
+                <th>AFECT.</th>
+                <th>TOTAL</th>
+              </tr> 
+              <tr className="row2table">
+                <td>Helobioma Altoandino cordillera oriental</td>
+                <td>7</td>
+                <td></td>
+                <td></td>
+                <td>2.19</td>
+                <td>0.3%</td>
+                <td>2.19</td>
+              </tr> 
+              <tr className="row2table">
+                <td>Helobioma Magdalena medio y depresión momposina</td>
+                <td>7.75</td>
+                <td>1.15</td>
+                <td>0.14</td>
+                <td>3.34</td>
+                <td>0.7%</td>
+                <td>12.83</td>
+              </tr> 
+              <tr className="row2table">
+                <td>Hidrobioma Altoandino cordillera oriental</td>
+                <td>5.25</td>
+                <td></td>
+                <td>0.58</td>
+                <td>0.02</td>
+                <td>0.1%</td>
+                <td>3.07</td>
+              </tr> 
+              <tr className="row2table">
+                <td>Hidrobioma Cordillera oriental Magdalena medio</td>
+                <td>4.25</td>
+                <td></td>
+                <td>1.86</td>
+                <td>2.78</td>
+                <td>0.7%</td>
+                <td>10.71</td>
+              </tr>
+              <tr className="row2table">
+                <td>Hidrobioma Cordillera oriental Magdalena medio</td>
+                <td>4.25</td>
+                <td></td>
+                <td>1.86</td>
+                <td>2.78</td>
+                <td>0.7%</td>
+                <td>10.71</td>
+              </tr>
+              <tr className="row2table">
+                <td>Hidrobioma Guane-Yariguíes</td>
+                <td>4.25</td>
+                <td></td>
+                <td></td>
+                <td>0.14</td>
+                <td>0.0%</td>
+                <td>0.14</td>
+              </tr>
+              <tr className="row2table">
+                <td>Hidrobioma Magdalena medio y depresión momposina</td>
+                <td>5.25</td>
+                <td></td>
+                <td>15.24</td>
+                <td>12.46</td>
+                <td>4.3%</td>
+                <td>92.47</td>
+              </tr>
+              <tr className="row2table">
+                <td>Hidrobioma Nechí-San Lucas</td>
+                <td>5.5</td>
+                <td>1.09</td>
+                <td></td>
+                <td>2.28</td>
+                <td>0.5%</td>
+                <td>8.32</td>
+              </tr>
+              <tr className="row2table">
+                <td>Orobioma Andino Altoandino cordillera oriental</td>
+                <td>7.75</td>
+                <td>38.62</td>
+                <td>1.63</td>
+                <td>114.60</td>
+                <td>23.8%</td>
+                <td>420.31</td>
+              </tr>
+              <tr className="row2table">
+                <td>Orobioma Andino Cordillera oriental Magdalena medio</td>
+                <td>8</td>
+                <td>6.40</td>
+                <td></td>
+                <td>26.22</td>
+                <td>5.0%</td>
+                <td>77.44</td>
+              </tr>
+              <tr className="row2table">
+                <td>Orobioma Andino Guane-Yariguíes</td>
+                <td>6.75</td>
+                <td>1.99</td>
+                <td>0.69</td>
+                <td>84.21</td>
+                <td>13.4%</td>
+                <td>100.03</td>
+              </tr>
+              <tr className="row2table">
+                <td>Orobioma Andino Tolima grande</td>
+                <td>7.25</td>
+                <td></td>
+                <td></td>
+                <td>1.22</td>
+                <td>0.2%</td>
+                <td>1.22</td>
+              </tr>
+              <tr className="row2table">
+                <td>Orobioma Azonal Andino Altoandino cordillera oriental</td>
+                <td>8.25</td>
+                <td>14.77</td>
+                <td></td>
+                <td>32.35</td>
+                <td>7.3%</td>
+                <td>154.27</td>
+              </tr>
+              <tr className="row2table">
+                <td>Orobioma Azonal Andino Cordillera oriental Magdalena medio</td>
+                <td>8</td>
+                <td></td>
+                <td></td>
+                <td>0.10</td>
+                <td>0.0%</td>
+                <td>0.10</td>
+              </tr>
+              <tr className="row2table">
+                <td>Orobioma Azonal Andino Tolima grande</td>
+                <td>8.25</td>
+                <td>2.63</td>
+                <td>2.91</td>
+                <td>6.17</td>
+                <td>1.8%</td>
+                <td>39.91</td>
+              </tr>
+              <tr className="row2table">
+                <td>Orobioma Azonal Subandino Cordillera oriental Magdalena medio</td>
+                <td>7.5</td>
+                <td></td>
+                <td>0.07</td>
+                <td>1.40</td>
+                <td>0.2%</td>
+                <td>1.70</td>
+              </tr>
+              <tr className="row2table">
+                <td>Orobioma Azonal Subandino Tolima grande</td>
+                <td>9</td>
+                <td></td>
+                <td>0.75</td>
+                <td>2.37</td>
+                <td>0.5%</td>
+                <td>5.75</td>
+              </tr>
+              <tr className="row2table">
+                <td>Orobioma de Paramo Altoandino cordillera oriental</td>
+                <td>6.25</td>
+                <td>9.70</td>
+                <td></td>
+                <td>3.60</td>
+                <td>2.0%</td>
+                <td>64.24</td>
+              </tr>
+              <tr className="row2table">
+                <td>Orobioma Subandino Altoandino cordillera oriental</td>
+                <td>8.5</td>
+                <td></td>
+                <td></td>
+                <td>2.93</td>
+                <td>0.5%</td>
+                <td>2.93</td>
+              </tr>
+              <tr className="row2table">
+                <td>Orobioma Subandino Cordillera oriental Magdalena medio</td>
+                <td>7.75</td>
+                <td>1.74</td>
+                <td></td>
+                <td>5.92</td>
+                <td>1.2%</td>
+                <td>19.40</td>
+              </tr>
+              <tr className="row2table">
+                <td>Orobioma Subandino Guane-Yariguíes</td>
+                <td>7.5</td>
+                <td>1.40</td>
+                <td>3.46</td>
+                <td>27.98</td>
+                <td>5.1%</td>
+                <td>51.50</td>
+              </tr>
+              <tr className="row2table">
+                <td>Zonobioma Humedo Tropical Cordillera oriental Magdalena medio</td>
+                <td>7.25</td>
+                <td>5.58</td>
+                <td>0.81</td>
+                <td>20.48</td>
+                <td>4.1%</td>
+                <td>63.93</td>
+              </tr>
+              <tr className="row2table">
+                <td>Zonobioma Humedo Tropical Guane-Yariguíes</td>
+                <td>7</td>
+                <td></td>
+                <td></td>
+                <td>3.52</td>
+                <td>0.5%</td>
+                <td>3.52</td>
+              </tr>
+              <tr className="row2table">
+                <td>Zonobioma Humedo Tropical Magdalena medio y depresión momposina</td>
+                <td>7.75</td>
+                <td>11.10</td>
+                <td>24.21</td>
+                <td>64.97</td>
+                <td>15.4%</td>
+                <td>244.86</td>
+              </tr>
+              <tr className="row2table">
+                <td>Zonobioma Humedo Tropical Nechí-San Lucas</td>
+                <td>7</td>
+                <td>21.40</td>
+                <td>3.11</td>
+                <td>55.19</td>
+                <td>12.3%</td>
+                <td>215.90</td>
+              </tr>
+              <tr className="row3table">
+                <td>TOTALES (CUANTO)</td>
+                <td></td>
+                <td>135.31</td>
+                <td>37.82</td>
+                <td>476.56</td>
+                <td>100.0%</td>
+                <td>1596.87</td>
+              </tr>
+            </table>
             {/* // tipoG="(Bullet Charts, https://bl.ocks.org/mbostock/4061961)"
             // datosJSON={this.props.datosJSON} */}
           </TabContainer>}
