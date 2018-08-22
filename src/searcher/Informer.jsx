ae292789daf3017acbba2d85a414d4613281fd65
Erika Suárez Valencia <marvinesv@gmail.com>
2018-08-22 09:24:11 -0500

    Add subtitle when a bioma is selected

--- src/searcher/Informer.jsx
+++ src/searcher/Informer.jsx
@@ -1,25 +1,33 @@
 import React from 'react';
-import Drawer from './Drawer';
 import BackIcon from '@material-ui/icons/FirstPage';
 
-class Informer extends React.Component {
-  render() {
-    return (
-      <div className="informer">
-      {/* TODO: Cambiar el zoom en el mapa para ver todo el país*/}
-      <button className="geobtn"
-        onClick={() => this.props.verMenu("Selector")}>
-        <BackIcon />
-      </button>
-          <div className="iconsection mt2"></div>
-          <h1> {this.props.geocerca} / {this.props.nombre} <br></br> <b>{this.props.subArea}</b></h1>
-          <Drawer
-            biomaActivo={this.props.biomaActivo}
-            biomaActivoData={this.props.biomaActivoData}
-          />
-      </div>
-    );
-  }
-}
+import Drawer from './Drawer';
+
+const Informer = ({
+  geocerca, nombre, verMenu, biomaActivo, biomaActivoData,
+}) => (
+  <div className="informer">
+    {/* TODO: Cambiar el zoom en el mapa para ver todo el país */}
+    <button
+      className="geobtn"
+      type="button"
+      onClick={() => verMenu('Selector')}
+    >
+      <BackIcon />
+    </button>
+    <div className="iconsection mt2" />
+    <h1>
+      {`${geocerca} / ${nombre}`}
+      <br />
+      <b>
+        {biomaActivo}
+      </b>
+    </h1>
+    <Drawer
+      biomaActivo={biomaActivo}
+      biomaActivoData={biomaActivoData}
+    />
+  </div>
+);
 
 export default Informer;
