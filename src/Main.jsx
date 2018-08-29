6d01774b2c7406900a9517e95b04648ad3527b5a
Erika Suárez Valencia <marvinesv@gmail.com>
2018-08-29 10:28:19 -0500

    DEL Remove unnecessary redirect from Main component DEL Remove LinkOption component

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,6 +1,6 @@
 /** eslint verified */
 import React from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import Home from './Home';
 import ShortInfo from './home/ShortInfo';
 import Search from './Search';
@@ -14,7 +14,6 @@ const Main = () => (
       <Route path="/Consultas" component={Search} />
       <Route path="/Compensaciones" component={Compensation} />
       <Route path="/Alertas" component={Home} />
-      <Redirect from="/" to="/Home" />
     </Switch>
   </main>
 );
