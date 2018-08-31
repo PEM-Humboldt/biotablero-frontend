27c1f1ddf493b2150583257a875f4341acf843ff
Erika Suárez Valencia <marvinesv@gmail.com>
2018-08-31 09:32:48 -0500

    Adjust Uim to eslint rules

--- src/header/Uim.jsx
+++ src/header/Uim.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 
-/* Uim: User Interface Manager*/
-class Uim extends React.Component {
+/* Uim: User Interface Manager */
+const Uim = () => (
+  <div>
+    <a
+      href="https://www.grupoenergiabogota.com/"
+      rel="noopener noreferrer"
+      target="_blank"
+      className="logoGEB"
+    >
+      <span />
+    </a>
+    <AccountCircle
+      className="userBox"
+      style={{ fontSize: '40px' }}
+    />
+  </div>
 
-  render(){
-    return(
-      <div>
-      	<a href="https://www.grupoenergiabogota.com/"
-        rel="noopener noreferrer"
-        target="_blank" className="logoGEB"> </a>
-        <AccountCircle className="userBox"
-          style={{fontSize: '40px'}}/>
-      </div>
-
-    )
-  }
-}
+);
 
 export default Uim;
