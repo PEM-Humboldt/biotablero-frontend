81b79d487b2b1d0aa1e1d18fb5f85312e31d933c
Erika Suárez Valencia <esuarez@humboldt.org.co>
2019-07-24 14:22:55 -0500

    Remove redirect for Indicators module

--- src/home/Content.jsx
+++ src/home/Content.jsx
@@ -20,7 +20,6 @@ const Content = ({ activeModule, setActiveModule, userLogged }) => (
       firstLineContent="indicadores de"
       secondLineContent="biodiversidad"
       localLink="/Indicadores"
-      externalLink="http://humboldt-156715.appspot.com/filters.html"
     />
     { userLogged ? ( // TODO: Implementing user validation
       <MenuButton
