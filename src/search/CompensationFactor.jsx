68822fb127ebfef860f4ddd399c459936c792c11
alegnaaived <alegnaaived@gmail.com>
2020-04-14 22:06:06 -0500

    Fix identation errors

--- src/search/CompensationFactor.jsx
+++ src/search/CompensationFactor.jsx
@@ -1,4 +1,4 @@
-/** eslint verified */
+
 import React from 'react';
 import RenderGraph from '../charts/RenderGraph';
 
@@ -18,27 +18,26 @@ const CompensationFactor = (
   <div className="graphcard">
     <div className="graphcontainer">
       <h4>
-                    hectáreas totales
+        hectáreas totales
         <b> 1,827,103.00 ha</b>
       </h4>
-
       {(areaName && areaName === 'Jurisdicciones ambientales')
-                    && RenderGraph(compensationFactorData, 'Hectáreas', 'F C', 'BarStackGraph', 'Factor de Compensación', compensationFactorColors, handlerInfoGraph, openInfoGraph, 'representa las hectáreas sobre los Biomas IAvH analizados')}
+        && RenderGraph(compensationFactorData, 'Hectáreas', 'F C', 'BarStackGraph', 'Factor de Compensación', compensationFactorColors, handlerInfoGraph, openInfoGraph, 'representa las hectáreas sobre los Biomas IAvH analizados')}
       {(areaName && areaName === 'Jurisdicciones ambientales')
-                    && RenderGraph(biomesData, 'Hectáreas', 'Biomas', 'BarStackGraph', 'Biomas', biomesColors, handlerInfoGraph, openInfoGraph, 'agrupa los biomas definidos a nivel nacional y presentes en esta área de consulta')}
+        && RenderGraph(biomesData, 'Hectáreas', 'Biomas', 'BarStackGraph', 'Biomas', biomesColors, handlerInfoGraph, openInfoGraph, 'agrupa los biomas definidos a nivel nacional y presentes en esta área de consulta')}
       {(areaName && areaName === 'Jurisdicciones ambientales')
-                    && RenderGraph(bioticRegionsData, 'Hectáreas', 'Regiones Bióticas', 'BarStackGraph', 'Regiones Bióticas', bioticRegionsColors, handlerInfoGraph, openInfoGraph, 'muestra las hectáreas por cada región biótica en el área de consulta seleccionada')}
+        && RenderGraph(bioticRegionsData, 'Hectáreas', 'Regiones Bióticas', 'BarStackGraph', 'Regiones Bióticas', bioticRegionsColors, handlerInfoGraph, openInfoGraph, 'muestra las hectáreas por cada región biótica en el área de consulta seleccionada')}
       {(areaName && areaName !== 'Jurisdicciones ambientales')
-                    && (
-                    <div className="graphcard">
-                      <h2>
-                            Gráficas en construcción
-                      </h2>
-                      <p>
-                            Pronto más información
-                      </p>
-                    </div>
-                    )}
+        && (
+          <div className="graphcard">
+            <h2>
+              Gráficas en construcción
+            </h2>
+            <p>
+              Pronto más información
+            </p>
+          </div>
+        )}
     </div>
   </div>
 );
