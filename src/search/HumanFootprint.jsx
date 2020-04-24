f7e537132639cbab9ea9cfef85fd8373463cfac5
Camilo Zapata <cazm013@gmail.com>
2020-04-24 15:04:12 -0500

    Fix ESLint errors in HumanFootprint.jsx

--- src/search/HumanFootprint.jsx
+++ src/search/HumanFootprint.jsx
@@ -14,8 +14,7 @@ const HumanFootprint = ({ generalArea, geofence }) => {
       disabled: false,
       expandIcon: <AddIcon />,
       detailId: 'Huella humana actual en área de consulta',
-      description: 'Huella humana identificada en el último año de medición disponible, '
-                + 'sobre el área de consulta',
+      description: 'Huella humana identificada en el último año de medición disponible, sobre el área de consulta',
     },
     component: (
       <CurrentFootprint
@@ -47,9 +46,7 @@ const HumanFootprint = ({ generalArea, geofence }) => {
       disabled: false,
       expandIcon: <AddIcon />,
       detailId: 'Persistencia de la huella humana en la unidad de consulta',
-      description: 'Representa la persistencia desde el origen del muestreo '
-                  + 'hasta el periodo actual, producto de análisis de huella'
-                  + 'humana en el tiempo y en esta área de consulta',
+      description: 'Representa la persistencia desde el origen del muestreo hasta el periodo actual, producto de análisis de huella humana en el tiempo y en esta área de consulta',
     },
     component: (
       <PersistenceFooprint
