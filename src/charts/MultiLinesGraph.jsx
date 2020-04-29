0ffea54dd0d9d5c5f53fcd76a238347da2c61931
alegnaaived <alegnaaived@gmail.com>
2020-04-29 16:38:56 -0500

    Add the multi lines graph using NIVO library

---
+++ src/charts/MultiLinesGraph.jsx
@@ -0,0 +1,98 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { ResponsiveLine } from '@nivo/line';
+
+const MultiLinesGraph = ({
+  data,
+}) => (
+  <div style={{ height: '400px', width: '750px' }}>
+    <ResponsiveLine
+      data={data}
+      curve="cardinal"
+      margin={{
+        top: 50,
+        left: 60,
+        right: 110,
+        bottom: 100,
+      }}
+      xScale={{ type: 'point' }}
+      yScale={{
+        type: 'linear',
+        min: 0,
+        max: 100,
+        stacked: false,
+        reverse: false,
+      }}
+      axisTop={null}
+      axisRight={null}
+      axisBottom={{
+        orient: 'bottom',
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: 0,
+        legend: 'Año',
+        legendOffset: 36,
+        legendPosition: 'middle',
+      }}
+      axisLeft={{
+        orient: 'left',
+        tickSize: 5,
+        tickPadding: 5,
+        tickRotation: 0,
+        legend: 'Indice promedio Huella Humana',
+        legendOffset: -40,
+        legendPosition: 'middle',
+      }}
+      colors={{ scheme: 'category10' }}
+      pointSize={10}
+      pointColor={{ theme: 'background' }}
+      pointBorderWidth={2}
+      pointBorderColor={{ from: 'serieColor' }}
+      pointLabel="y"
+      pointLabelYOffset={-12}
+      useMesh
+      legends={[
+        {
+          anchor: 'bottom-right',
+          direction: 'column',
+          justify: false,
+          translateX: 100,
+          translateY: 0,
+          itemsSpacing: 0,
+          itemDirection: 'left-to-right',
+          itemWidth: 80,
+          itemHeight: 20,
+          itemOpacity: 0.75,
+          symbolSize: 12,
+          symbolShape: 'circle',
+          symbolBorderColor: 'rgba(0, 0, 0, .5)',
+          effects: [
+            {
+              on: 'hover',
+              style: {
+                itemBackground: 'rgba(0, 0, 0, .03)',
+                itemOpacity: 1,
+              },
+            },
+          ],
+        },
+      ]}
+    />
+  </div>
+);
+
+MultiLinesGraph.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string,
+    color: PropTypes.string,
+    data: PropTypes.arrayOf(PropTypes.shape({
+      x: PropTypes.string,
+      y: PropTypes.number,
+    })),
+  })).isRequired,
+};
+
+MultiLinesGraph.defaultProps = {
+};
+
+export default MultiLinesGraph;
