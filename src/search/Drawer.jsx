817cfe16645c118ef290d4c26b70816d034d37c9
Erika Suárez Valencia <esuarez@humboldt.org.co>
2020-05-08 10:46:14 -0500

    Fix indentation

--- src/search/Drawer.jsx
+++ src/search/Drawer.jsx
@@ -207,40 +207,41 @@ class Drawer extends React.Component {
     } = this.state;
     const ecosystemsArea = (areaSE && areaSE[0] ? Number(areaSE[0].area).toFixed(2) : 0);
     const protectedArea = (areaPA && areaPA[0] ? Number(areaPA[0].area).toFixed(2) : 0);
-    const componentsArray = [{
-      label: {
-        id: 'Factor de compensación',
-        name: 'Factor de compensación',
-        disabled: false,
-        expandIcon: <AddIcon />,
-        detailId: 'Factor de compensación en área de consulta',
-        description: 'Representa el coeficiente de relación entre BiomasIAvH y regiones bióticas',
+    const componentsArray = [
+      {
+        label: {
+          id: 'Factor de compensación',
+          name: 'Factor de compensación',
+          disabled: false,
+          expandIcon: <AddIcon />,
+          detailId: 'Factor de compensación en área de consulta',
+          description: 'Representa el coeficiente de relación entre BiomasIAvH y regiones bióticas',
+        },
+        component: <CompensationFactor
+          areaName={area.name}
+          biomesData={biomas}
+          bioticRegionsData={distritos}
+          compensationFactorData={fc}
+          handlerInfoGraph={handlerInfoGraph}
+          openInfoGraph={openInfoGraph}
+          matchColor={matchColor}
+        />,
       },
-      component: <CompensationFactor
-        areaName={area.name}
-        biomesData={biomas}
-        bioticRegionsData={distritos}
-        compensationFactorData={fc}
-        handlerInfoGraph={handlerInfoGraph}
-        openInfoGraph={openInfoGraph}
-        matchColor={matchColor}
-      />,
-    },
-    {
-      label: {
-        id: 'Huella humana',
-        name: 'Huella humana',
-        disabled: false,
-        expandIcon: <AddIcon />,
-        detailId: 'Huella humana en el área',
-        description: 'Representa diferentes análisis de huella humana en esta área de consulta',
+      {
+        label: {
+          id: 'Huella humana',
+          name: 'Huella humana',
+          disabled: false,
+          expandIcon: <AddIcon />,
+          detailId: 'Huella humana en el área',
+          description: 'Representa diferentes análisis de huella humana en esta área de consulta',
+        },
+        component: (
+          <HumanFootprint
+            generalArea={generalArea}
+          />
+        ),
       },
-      component: (
-        <HumanFootprint
-          generalArea={generalArea}
-        />
-      ),
-    },
     ];
     return (
       <div className="informer">
