17f27b7f4f5f2fb71f0cde5494dfcaad98ca1a37
alegnaaived <alegnaaived@gmail.com>
2020-05-10 20:44:06 -0500

    Remove SmallBarStackGraph and data template to use in this removed graph

--- src/humanFootprint/PersistenceFootprint.jsx
+++ src/humanFootprint/PersistenceFootprint.jsx
@@ -4,16 +4,6 @@ import RenderGraph from '../charts/RenderGraph';
 import GeneralArea from '../commons/GeneralArea';
 import matchColor from '../commons/matchColor';
 
-const data = [
-  {
-    area: 732206, percentage: 0.29405913098887474, type: 'Estable natural', color: '#5aa394',
-  }, {
-    area: 70749, percentage: 0.03807574570316536, type: 'Dinámica', color: '#fb6a2a',
-  }, {
-    area: 1054399, percentage: 0.5674571823442289, type: 'Estable alta', color: '#b3433b',
-  },
-];
-
 const PersistenceFootprint = ({ generalArea }) => (
   <div className="graphcontainer pt5">
     <GeneralArea
@@ -26,18 +16,6 @@ const PersistenceFootprint = ({ generalArea }) => (
       Estable natural, Dinámica, Estable alta
     </h6>
     <div className="graficaeco">
-      <RenderGraph
-        graph="SmallBarStackGraph"
-        data={data}
-        graphTitle="Cobertura"
-        colors={null}
-        labelX="Tipo de área"
-        labelY="Comparación"
-        handlerInfoGraph={null}
-        openInfoGraph={null}
-        graphDescription="Estado de la cobertura en el área seleccionada"
-        units="%"
-      />
       <RenderGraph
         graph="LargeBarStackGraph"
         data={[
