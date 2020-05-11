1834d8d980f694e486bf809cb8ba74c777f9ce1c
alegnaaived <alegnaaived@gmail.com>
2020-05-10 21:06:24 -0500

    Remove old SmallBarStackGraph and data mockup used in previous graph

--- src/humanFootprint/CurrentFootprint.jsx
+++ src/humanFootprint/CurrentFootprint.jsx
@@ -17,27 +17,6 @@ const CurrentFootprint = ({ generalArea }) => (
     </h6>
     <div className="graficaeco">
       <RenderGraph
-        graph="SmallBarStackGraph"
-        data={[
-          {
-            area: 732206, percentage: 0.29405913098887474, type: 'Natural', color: '#5aa394',
-          }, {
-            area: 70749, percentage: 0.03807574570316536, type: 'Baja', color: '#dea857',
-          }, {
-            area: 533399, percentage: 0.3674571823442289, type: 'Media', color: '#fb6a2a',
-          }, {
-            area: 521758, percentage: 0.30040794096373092685, type: 'Alta', color: '#b3433b',
-          }]}
-        graphTitle="Cobertura"
-        colors={null}
-        labelX="Tipo de área"
-        labelY="Comparación"
-        handlerInfoGraph={null}
-        openInfoGraph={null}
-        graphDescription="Estado de la cobertura en el área seleccionada"
-        units="%"
-      />
-      <RenderGraph
         graph="LargeBarStackGraph"
         data={[
           {
