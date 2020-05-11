f4ce2314e9a99a22400c234de6a2828e6c5485df
alegnaaived <alegnaaived@gmail.com>
2020-05-10 21:02:40 -0500

    Add new graph using NIVO

--- src/humanFootprint/CurrentFootprint.jsx
+++ src/humanFootprint/CurrentFootprint.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import RenderGraph from '../charts/RenderGraph';
 import GeneralArea from '../commons/GeneralArea';
+import matchColor from '../commons/matchColor';
 
 const CurrentFootprint = ({ generalArea }) => (
   <div className="graphcontainer pt5">
@@ -35,6 +36,27 @@ const CurrentFootprint = ({ generalArea }) => (
         openInfoGraph={null}
         graphDescription="Estado de la cobertura en el área seleccionada"
         units="%"
+      />
+      <RenderGraph
+        graph="LargeBarStackGraph"
+        data={[
+          {
+            area: 732206, key: 'Natural',
+          }, {
+            area: 70749, key: 'Baja',
+          }, {
+            area: 533399, key: 'Media',
+          }, {
+            area: 521758, key: 'Alta',
+          }]}
+        graphTitle="Cobertura"
+        labelX="Hectáreas"
+        labelY="Cobertura"
+        handlerInfoGraph={null}
+        openInfoGraph={null}
+        graphDescription="representa las hectáreas sobre los tipos de cobertura de huella humana identificados"
+        zScale={matchColor('bioticReg')}
+        padding={0.25}
       />
     </div>
   </div>
