2b650465ab7f2ce7e7212daf9353ef30d3f35424
alegnaaived <alegnaaived@gmail.com>
2020-06-01 22:51:18 -0500

    Move changeValues from selectedData to TimelineFootprint component

--- src/humanFootprint/TimelineFootprint.jsx
+++ src/humanFootprint/TimelineFootprint.jsx
@@ -4,7 +4,62 @@ import GeneralArea from '../commons/GeneralArea';
 import MultiLinesGraph from '../charts/MultiLinesGraph';
 import matchColor from '../commons/matchColor';
 
-import { changeValues, dataLines } from '../search/assets/selectorData';
+import { dataLines } from '../search/assets/selectorData';
+
+const changeValues = [
+  {
+    axis: 'y',
+    value: 15,
+    legend: 'Natural',
+    lineStyle: { stroke: '#3fbf9f', strokeWidth: 1 },
+    textStyle: {
+      fill: '#3fbf9f',
+      fontSize: 9,
+    },
+    legendPosition: 'bottom-right',
+    orient: 'top',
+    tickRotation: -90,
+  },
+  {
+    axis: 'y',
+    value: 30,
+    legend: 'Baja',
+    lineStyle: { stroke: '#d5a529', strokeWidth: 1 },
+    textStyle: {
+      fill: '#d5a529',
+      fontSize: 9,
+    },
+    legendPosition: 'bottom-right',
+    orient: 'top',
+    tickRotation: -90,
+  },
+  {
+    axis: 'y',
+    value: 60,
+    legend: 'Media',
+    lineStyle: { stroke: '#e66c29', strokeWidth: 1 },
+    textStyle: {
+      fill: '#e66c29',
+      fontSize: 9,
+    },
+    legendPosition: 'bottom-right',
+    orient: 'top',
+    tickRotation: -90,
+  },
+  {
+    axis: 'y',
+    value: 100,
+    legend: 'Alta',
+    lineStyle: { stroke: '#cf324e', strokeWidth: 1 },
+    textStyle: {
+      fill: '#cf324e',
+      fontSize: 9,
+    },
+    legendPosition: 'bottom-right',
+    orient: 'top',
+    tickRotation: -90,
+  },
+];
 
 const TimelineFootprint = ({ generalArea, selection, setSelection }) => (
   <div className="graphcontainer pt5">
