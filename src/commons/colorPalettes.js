ec9c8e7d8faa4700c7b77d1639080bd28e336f71
alegnaaived <alegnaaived@gmail.com>
2020-06-04 17:12:16 -0500

    Remove unused palette currentHFPLower

--- src/commons/colorPalettes.js
+++ src/commons/colorPalettes.js
@@ -92,12 +92,6 @@ export default {
     '#e66c29',
     '#cf324e',
   ],
-  currentHFPLower: [
-    '#addccd',
-    '#eacf94',
-    '#f2b48a',
-    '#e49c98',
-  ],
   persistenceHFP: [
     '#3fbf9f',
     '#646e55',
