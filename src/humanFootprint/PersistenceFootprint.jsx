855262a058b58a5bdccf70b5f7b9f0798c8b02b4
Camilo Z <cazm013@gmail.com>
2020-06-08 18:56:24 -0500

    Delete props to show and hide additional info on the graph and setting a state on the component PersistenceFootprint

--- src/humanFootprint/PersistenceFootprint.jsx
+++ src/humanFootprint/PersistenceFootprint.jsx
@@ -5,69 +5,72 @@ import ShortInfo from '../commons/ShortInfo';
 import GraphLoader from '../charts/GraphLoader';
 import matchColor from '../commons/matchColor';
 
-const PersistenceFootprint = (props) => {
-  const {
-    data,
-    handlerInfoGraph,
-    openInfoGraph,
-  } = props;
-  return (
-    <div className="graphcontainer pt6">
-      <h2>
-        <InfoIcon
-          className="graphinfo"
-          data-tooltip
-          title="¿Qué significa este gráfico?"
-          onClick={() => {
-            handlerInfoGraph('PersistenceFootprint');
-          }}
-        />
-        <div
-          className="graphinfo"
-          onClick={() => handlerInfoGraph('PersistenceFootprint')}
-          onKeyPress={() => handlerInfoGraph('PersistenceFootprint')}
-          role="button"
-          tabIndex="0"
-        />
-      </h2>
-      {(
-        openInfoGraph && (openInfoGraph === 'PersistenceFootprint') && (
-        <ShortInfo
-          name="Persistencia Huella Humana"
-          description="Barra horizontal del mismo tipo a la de la sección 1 (o a las barras horizontales existentes en BioTablero) que muestra el área total de la unidad de consulta distribuida en los diferentes valores de persistencia"
-          className="graphinfo2"
-          tooltip="¿Qué significa?"
-          customButton
-        />
-        )
-      )}
-      <h6>
-        Estable natural, Dinámica, Estable alta
-      </h6>
-      <div>
-        <GraphLoader
-          graphType="LargeBarStackGraph"
-          data={data}
-          labelX="Hectáreas"
-          labelY="Persistencia Huella Humana"
-          units="ha"
-          colors={matchColor('persistenceHFP')}
-          padding={0.25}
-        />
+class PersistenceFootprint extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showInfoGraph: false,
+    };
+  }
+
+  toggleInfoGraph = () => {
+    this.setState(prevState => ({
+      showInfoGraph: !prevState.showInfoGraph,
+    }));
+  };
+
+  render() {
+    const { data } = this.props;
+    const { showInfoGraph } = this.state;
+    return (
+      <div className="graphcontainer pt6">
+        <h2>
+          <InfoIcon
+            className="graphinfo"
+            data-tooltip
+            title="¿Qué significa este gráfico?"
+            onClick={() => this.toggleInfoGraph()}
+          />
+          <div
+            className="graphinfo"
+            onClick={() => this.toggleInfoGraph()}
+            onKeyPress={() => this.toggleInfoGraph()}
+            role="button"
+            tabIndex="0"
+          />
+        </h2>
+        {(
+          showInfoGraph && (
+          <ShortInfo
+            name="Persistencia Huella Humana"
+            description="Barra horizontal del mismo tipo a la de la sección 1 (o a las barras horizontales existentes en BioTablero) que muestra el área total de la unidad de consulta distribuida en los diferentes valores de persistencia"
+            className="graphinfo2"
+            tooltip="¿Qué significa?"
+            customButton
+          />
+          )
+        )}
+        <h6>
+          Estable natural, Dinámica, Estable alta
+        </h6>
+        <div>
+          <GraphLoader
+            graphType="LargeBarStackGraph"
+            data={data}
+            labelX="Hectáreas"
+            labelY="Persistencia Huella Humana"
+            units="ha"
+            colors={matchColor('persistenceHFP')}
+            padding={0.25}
+          />
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 PersistenceFootprint.propTypes = {
   data: PropTypes.array.isRequired,
-  handlerInfoGraph: PropTypes.func,
-  openInfoGraph: PropTypes.string,
-};
-
-PersistenceFootprint.defaultProps = {
-  handlerInfoGraph: () => {},
-  openInfoGraph: null,
 };
 
 export default PersistenceFootprint;
