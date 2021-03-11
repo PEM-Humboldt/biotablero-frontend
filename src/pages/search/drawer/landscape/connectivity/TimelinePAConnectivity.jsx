8406939466d663b34cb95ba6eb313865427233b3
Trjegul84 <angelachek@gmail.com>
2021-03-11 01:30:55 -0500

    Call component to render timeline graph and call backend RestAPI

--- src/pages/search/drawer/landscape/connectivity/TimelinePAConnectivity.jsx
+++ src/pages/search/drawer/landscape/connectivity/TimelinePAConnectivity.jsx
@@ -1,5 +1,27 @@
 import React from 'react';
 
+import ShortInfo from 'components/ShortInfo';
+import { IconTooltip } from 'components/Tooltips';
+import InfoIcon from '@material-ui/icons/Info';
+import GraphLoader from 'components/charts/GraphLoader';
+import matchColor from 'utils/matchColor';
+import RestAPI from 'utils/restAPI';
+
+const changeValues = [
+  {
+    axis: 'y',
+    value: 80,
+    lineStyle: { stroke: '#3fbf9f', strokeWidth: 1 },
+    textStyle: {
+      fill: '#3fbf9f',
+      fontSize: 9,
+    },
+    legendPosition: 'bottom-right',
+    orient: 'top',
+    tickRotation: -90,
+  },
+];
+
 class TimelinePAConnectivity extends React.Component {
   mounted = false;
 
@@ -7,11 +29,22 @@ class TimelinePAConnectivity extends React.Component {
     super(props);
     this.state = {
       showInfoGraph: false,
+      timelinePAConnectivity: [],
     };
   }
 
   componentDidMount() {
     this.mounted = true;
+    const { areaId, geofenceId } = this.context;
+    Promise.all([
+      RestAPI.requestTimelinePAConnectivity(areaId, geofenceId, 'prot'),
+      RestAPI.requestTimelinePAConnectivity(areaId, geofenceId, 'prot_conn'),
+    ])
+      .then(([prot, protConn]) => {
+        if (this.mounted) {
+          this.setState({ timelinePAConnectivity: prot.concat(protConn) });
+        }
+      });
   }
 
   componentWillUnmount() {
@@ -28,12 +61,47 @@ class TimelinePAConnectivity extends React.Component {
   };
 
   render() {
+    const {
+      showInfoGraph,
+      timelinePAConnectivity,
+    } = this.state;
     return (
       <div className="graphcontainer pt6">
-        <div />
+        <h2>
+          <IconTooltip title="Acerca de esta sección">
+            <InfoIcon
+              className="graphinfo"
+              onClick={() => this.toggleInfoGraph()}
+            />
+          </IconTooltip>
+        </h2>
+        {(
+          showInfoGraph && (
+            <ShortInfo
+              description="Timeline PA Connectivity"
+              className="graphinfo2"
+              collapseButton={false}
+            />
+          )
+        )}
+        <div>
+          <h6>
+            Conectividad áreas protegidas en el tiempo
+          </h6>
+          <div>
+            <GraphLoader
+              graphType="MultiLinesGraph"
+              colors={matchColor('timelinePAConn')}
+              data={timelinePAConnectivity}
+              markers={changeValues}
+              labelX="Año"
+              labelY="Porcentaje"
+            />
+          </div>
+        </div>
       </div>
     );
-  }
+  }//
 }
 
 export default TimelinePAConnectivity;
