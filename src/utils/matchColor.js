ff112d92ebf3977ce18a2fcc5a1a66823acb5bc5
Trjegul84 <angelachek@gmail.com>
2021-03-15 15:44:09 -0500

    add two more colors for highlighted lines

--- src/utils/matchColor.js
+++ src/utils/matchColor.js
@@ -93,7 +93,7 @@ const match = {
   },
   timelinePAConn: {
     palette: 'timelinePAConn',
-    sort: ['prot', 'prot_conn'],
+    sort: ['prot', 'protSel', 'prot_conn', 'prot_connSel'],
   },
   border: {
     palette: 'border',
