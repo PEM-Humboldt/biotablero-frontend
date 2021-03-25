fa8ebc912f817901f5e2f763634b2dc8a021382d
Erika Suárez Valencia <esuarez@humboldt.org.co>
2021-03-24 20:05:58 -0500

    Adjust active card

--- packages/cbm-dashboard/src/app/MethodologyBoard.jsx
+++ packages/cbm-dashboard/src/app/MethodologyBoard.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import ThumbDownAltIcon from '@material-ui/icons/ThumbDownAlt';
 import PropTypes from 'prop-types';
@@ -8,19 +8,53 @@ import M02 from './M02';
 import M06 from './M06';
 
 const MethodologyBoard = ({ methodology }) => {
+  const [activeTab, setActiveTab] = useState('GENERAL');
+
   return (
     <>
-      <div className="Montabs1 cardTab cTactive">GENERAL</div>
-      <div className="Montabs2 cardTab">AMUSI</div>
-      <div className="Montabs3 cardTab">ASICAC</div>
-      <div className="Montabs4 cardTab">ASOBRASILAR</div>
+      <div
+        className={`Montabs1 cardTab ${activeTab === 'GENERAL' ? 'cTactive' : ''}`}
+        onClick={() => setActiveTab('GENERAL')}
+        onKeyDown={() => setActiveTab('GENERAL')}
+        role="button"
+        tabIndex={0}
+      >
+        GENERAL
+      </div>
+      <div
+        className={`Montabs2 cardTab ${activeTab === 'AMUSI' ? 'cTactive' : ''}`}
+        onClick={() => setActiveTab('AMUSI')}
+        onKeyDown={() => setActiveTab('AMUSI')}
+        role="button"
+        tabIndex={0}
+      >
+        AMUSI
+      </div>
+      <div
+        className={`Montabs3 cardTab ${activeTab === 'ASICAC' ? 'cTactive' : ''}`}
+        onClick={() => setActiveTab('ASICAC')}
+        onKeyDown={() => setActiveTab('ASICAC')}
+        role="button"
+        tabIndex={0}
+      >
+        ASICAC
+      </div>
+      <div
+        className={`Montabs4 cardTab ${activeTab === 'ASOBRASILAR' ? 'cTactive' : ''}`}
+        onClick={() => setActiveTab('ASOBRASILAR')}
+        onKeyDown={() => setActiveTab('ASOBRASILAR')}
+        role="button"
+        tabIndex={0}
+      >
+        ASOBRASILAR
+      </div>
       <div className="Graph card">
-        {methodology === '01_validacion_coberturas' && <M01 />}
-        {methodology === '02_parcela_vegetacion' && <M02 />}
-        {methodology === '06_medicion_lluvia' && <M06 />}
+        {methodology.id === '01_validacion_coberturas' && <M01 />}
+        {methodology.id === '02_parcela_vegetacion' && <M02 />}
+        {methodology.id === '06_medicion_lluvia' && <M06 />}
       </div>
       <div className="graphInfo card">
-        <h2>Disturbios</h2>
+        <h2>{methodology.name}</h2>
         <h3>
           Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod
           tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam,
@@ -28,20 +62,25 @@ const MethodologyBoard = ({ methodology }) => {
           consequat.
         </h3>
         <h2>
-          Periodicidad <ThumbUpAltIcon />
+          Periodicidad <ThumbUpAltIcon /> <ThumbDownAltIcon />
         </h2>
-        <ThumbDownAltIcon />
       </div>
     </>
   );
 };
 
 MethodologyBoard.propTypes = {
-  methodology: PropTypes.string,
+  methodology: PropTypes.shape({
+    id: PropTypes.string,
+    name: PropTypes.string,
+  }),
 };
 
 MethodologyBoard.defaultProps = {
-  methodology: '',
+  methodology: {
+    id: '',
+    name: '',
+  },
 };
 
 export default MethodologyBoard;
