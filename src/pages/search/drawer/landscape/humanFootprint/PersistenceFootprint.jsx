373d1ef0a841b85fc10252c8712b078bb07d3911
Camilo Zapata <cazm013@gmail.com>
2021-05-27 10:18:19 -0500

    Fix sections names all in english

--- src/pages/search/drawer/landscape/humanFootprint/PersistenceFootprint.jsx
+++ src/pages/search/drawer/landscape/humanFootprint/PersistenceFootprint.jsx
@@ -92,7 +92,7 @@ class PersistenceFootprint extends React.Component {
         {(hfPersistence && hfPersistence.length > 0) && (
           <DownloadCSV
             data={hfPersistence}
-            filename={`bt_hf_persistencia_${areaId}_${geofenceId}.csv`}
+            filename={`bt_hf_persistence_${areaId}_${geofenceId}.csv`}
           />
         )}
         <div>
