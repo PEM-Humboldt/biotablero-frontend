ecc7c3dfee53d0b6647d6b78e9e4637608cc81d2
Camilo Zapata <cazm013@gmail.com>
2021-07-15 18:26:11 -0500

    First version of graphs to functional diversity dry forest

--- src/pages/search/drawer/species/functionalDiversity/TropicalDryForest.jsx
+++ src/pages/search/drawer/species/functionalDiversity/TropicalDryForest.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
 
+import InfoIcon from '@material-ui/icons/Info';
+import { IconTooltip } from 'components/Tooltips';
+import GraphLoader from 'components/charts/GraphLoader';
+import ShortInfo from 'components/ShortInfo';
+import SearchContext from 'pages/search/SearchContext';
+import RestAPI from 'utils/restAPI';
+import matchColor from 'utils/matchColor';
+
+const getFeatureLabel = {
+  leaf_area: 'Área Foliar',
+  leaf_nitrogen: 'Nitrógeno foliar',
+  maximun_height: 'Altura Máxima',
+  specific_leaf_area: 'Área Foliar Específica',
+  wood_density: 'Densidad de Madera',
+  seed_mass: 'Masa de Semilla',
+};
+
 class TropicalDryForest extends React.Component {
   mounted = false;
 
@@ -7,16 +24,78 @@ class TropicalDryForest extends React.Component {
     super(props);
     this.state = {
       showInfoGraph: false,
+      values: {},
+      features: [],
+      message: 'loading',
+      selected: null,
     };
   }
 
   componentDidMount() {
     this.mounted = true;
+    const {
+      areaId,
+      geofenceId,
+    } = this.context;
+
+    RestAPI.requestDryForestValues(areaId, geofenceId)
+      .then((res) => {
+        if (this.mounted) {
+          this.setState({
+            values: res,
+            message: null,
+          });
+        }
+      })
+      .catch(() => {});
+
+    RestAPI.requestDryForestFeatures(areaId, geofenceId)
+      .then((res) => {
+        if (this.mounted) {
+          this.setState({
+            features: this.transformData(res),
+            message: null,
+          });
+        }
+      })
+      .catch(() => {});
   }
 
   componentWillUnmount() {
     this.mounted = false;
   }
+
+  /**
+   * Transform data structure to be passed to graph component as a prop
+   *
+   * @param {Object} rawData raw data from RestAPI
+   */
+   transformData = (rawData) => {
+    const tranformedData = [];
+    rawData.forEach((obj) => {
+      const {
+        id,
+        min,
+        max,
+        value,
+      } = obj;
+      tranformedData.push({
+        id,
+        ranges: {
+          area: max,
+        },
+        markers: {
+          value,
+        },
+        measures: {
+          min,
+          max,
+        },
+        title: '',
+      });
+    });
+    return tranformedData;
+  };
 
   /**
    * Show or hide the detailed information on each graph
@@ -28,12 +107,93 @@ class TropicalDryForest extends React.Component {
   };
 
   render() {
+    const {
+      showInfoGraph,
+      message,
+      values,
+      features,
+      selected,
+    } = this.state;
     return (
       <div className="graphcontainer pt6">
-        <div />
+        <h2>
+          <IconTooltip title="Acerca de esta sección">
+            <InfoIcon
+              className="graphinfo"
+              onClick={() => this.toggleInfoGraph()}
+            />
+          </IconTooltip>
+        </h2>
+        {(
+          showInfoGraph && (
+          <ShortInfo
+            name="Plantas del bosque seco"
+            description="Plantas del bosque seco"
+            className="graphinfo2"
+            collapseButton={false}
+          />
+          )
+        )}
+        <div>
+          <h6>
+            Riqueza
+          </h6>
+          <h5 style={{ backgroundColor: matchColor('functionalDryForest')('value') }}>
+            {values.richness}
+          </h5>
+          <h6>
+            Uniformidad
+          </h6>
+          <h5 style={{ backgroundColor: matchColor('functionalDryForest')('value') }}>
+            {values.uniformity}
+          </h5>
+          <h6>
+            Divergencia
+          </h6>
+          <h5 style={{ backgroundColor: matchColor('functionalDryForest')('value') }}>
+            {values.divergence}
+          </h5>
+        </div>
+        <h3>Haga click en la barra para visualizar su mapa</h3>
+        <div>
+          <h6>
+            Rasgos funcionales
+          </h6>
+          <br />
+          <br />
+          {message === 'no-data' && (
+            <GraphLoader
+              message={message}
+              data={[]}
+              graphType="singleBullet"
+            />
+          )}
+          {features.map((bar) => (
+            <div key={bar.id}>
+              <div
+                className={`nos-title${bar.id === selected ? ' selected' : ''}`}
+              >
+                {getFeatureLabel[bar.id]}
+              </div>
+              <div className="svgPointer">
+                <GraphLoader
+                  message={message}
+                  data={bar}
+                  graphType="singleBullet"
+                  colors={matchColor('functionalDryForest')}
+                  onClickGraphHandler={() => {
+                    this.setState({ selected: bar.id });
+                  }}
+                />
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     );
   }
 }
 
 export default TropicalDryForest;
+
+TropicalDryForest.contextType = SearchContext;
