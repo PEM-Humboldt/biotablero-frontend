bb9b3b8a569756a6029929634bdd2dab69b0f11a
Erika Suárez Valencia <1734998+erikasv@users.noreply.github.com>
2021-07-14 19:08:48 -0500

    Restore deleted function

--- src/utils/restAPI.js
+++ src/utils/restAPI.js
@@ -406,19 +406,17 @@ class RestAPI {
   /** ******************** */
 
   /**
-   * Request data available to custom geometry
+   * Request the geometry of the biomes by EA
+   * @param {String} eaId id ea to request
    *
-   * @param {Object} polygon polygon saved
-   *
-   * @return {Object} with data related to the polygon
+   * @return {Promise<Object>} layer object to be loaded in the map
    */
-  static requestCustomPolygonData(polygon) {
-    /** TODO: implement all this endpoint and also the backend response
-     * to find information according to polygon coordinates
-     * */
-    return RestAPI.makePostRequest('polygon', {
-      latLngs: polygon.latLngs,
-    });
+   static requestBiomesbyEAGeometry(eaId) {
+    const source = CancelToken.source();
+    return {
+      request: RestAPI.makeGetRequest(`ea/layers/${eaId}/biomes`, { cancelToken: source.token }),
+      source,
+    };
   }
 
   /**
@@ -673,6 +671,22 @@ class RestAPI {
     return RestAPI.makeGetRequest(
       `richness/number-species/layer/thresholds?areaType=${areaType}&areaId=${areaId}&group=${group}`,
     );
+  }
+
+  /**
+   * Request data available to custom geometry
+   *
+   * @param {Object} polygon polygon saved
+   *
+   * @return {Object} with data related to the polygon
+   */
+   static requestCustomPolygonData(polygon) {
+    /** TODO: implement all this endpoint and also the backend response
+     * to find information according to polygon coordinates
+     * */
+    return RestAPI.makePostRequest('polygon', {
+      latLngs: polygon.latLngs,
+    });
   }
 
   /** ******************* */
