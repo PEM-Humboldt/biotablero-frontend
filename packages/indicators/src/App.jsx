3a8a0605062bff5e93eca522484d019e3e34abfb
Angela Devia <alegnaaived@gmail.com>
2021-09-08 22:05:33 -0500

    Divide indicators in two sections

--- packages/indicators/src/App.jsx
+++ packages/indicators/src/App.jsx
@@ -4,15 +4,37 @@ import './main.css';
 
 const App = () => {
   const [stateVal, setStateVal] = useState(1);
+  const [openFilter, setOpenFilter] = useState(true);
 
   return (
-    <div className="wrapperIndicators">
-      <div>Este es un ejemplo de estado con hooks :D: {stateVal}</div>
+    <div className="wrapper">
       <div>
-        Aumenta el estado:{' '}
-        <button type="button" onClick={() => setStateVal(stateVal + 1)}>
-          ++
-        </button>
+        <h3>
+          <button type="button" onClick={() => setOpenFilter(!openFilter)}>
+            {openFilter ? '-' : '+'}
+          </button>
+          Filtros de búsqueda{' '}
+        </h3>
+        {openFilter && (
+          <div>
+            <h4>
+              <button type="button" onClick={() => setOpenFilter(!openFilter)}>
+                x
+              </button>
+              8 filtros
+            </h4>
+          </div>
+        )}
+      </div>
+      <div className="wrapperIndicators">
+        <div>27 indicadores</div>
+        <div>Este es el ejemplo de estado con hooks :D: {stateVal}</div>
+        <div>
+          Aumenta el estado:{' '}
+          <button type="button" onClick={() => setStateVal(stateVal + 1)}>
+            ++
+          </button>
+        </div>
       </div>
     </div>
   );
