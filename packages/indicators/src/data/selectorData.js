cfda47b120b16d131a0f84ebd7a47af3473ff5a6
Angela Devia <alegnaaived@gmail.com>
2021-09-13 16:29:48 -0500

    Set test data and card component

---
+++ packages/indicators/src/data/selectorData.js
@@ -0,0 +1,61 @@
+const cardsData = [
+  {
+    id: '1',
+    title: 'Porcentaje de cobertura boscosa',
+    description:
+      'Forest area as a percentage of total land area (proposed indictor for SDG target 15.1)',
+    period: '1990 - 2014',
+    filters: ['Aichi 5', 'Estado', 'Ecosistemas Cobertura'],
+  },
+  {
+    id: '2',
+    title: 'Porcentaje de cobertura natural',
+    description: 'Natural habitat extent (land area minus urban and agriculture)',
+    period: '2005 - 2012',
+    filters: ['Aichi 5', 'Estado', 'Ecosistemas Cobertura'],
+  },
+  {
+    id: '3',
+    title: '3 Porcentaje de cobertura natural',
+    description: 'Natural habitat extent (land area minus urban and agriculture)',
+    period: '2005 - 2012',
+    filters: ['Aichi 5', 'Estado', 'Ecosistemas Cobertura'],
+  },
+  {
+    id: '4',
+    title: '4 Porcentaje de cobertura natural',
+    description: 'Natural habitat extent (land area minus urban and agriculture)',
+    period: '2005 - 2012',
+    filters: ['Aichi 5', 'Estado', 'Ecosistemas Cobertura'],
+  },
+  {
+    id: '5',
+    title: '5 Porcentaje de cobertura natural',
+    description: 'Natural habitat extent (land area minus urban and agriculture)',
+    period: '2005 - 2012',
+    filters: ['Aichi 5', 'Estado', 'Ecosistemas Cobertura'],
+  },
+  {
+    id: '6',
+    title: '6 Porcentaje de cobertura natural',
+    description: 'Natural habitat extent (land area minus urban and agriculture)',
+    period: '2005 - 2012',
+    filters: ['Aichi 5', 'Estado', 'Ecosistemas Cobertura'],
+  },
+  {
+    id: '7',
+    title: '7 Porcentaje de cobertura natural',
+    description: 'Natural habitat extent (land area minus urban and agriculture)',
+    period: '2005 - 2012',
+    filters: ['Aichi 5', 'Estado', 'Ecosistemas Cobertura'],
+  },
+  {
+    id: '8',
+    title: '',
+    description: '',
+    period: '',
+    filters: [''],
+  },
+];
+
+export default cardsData;
