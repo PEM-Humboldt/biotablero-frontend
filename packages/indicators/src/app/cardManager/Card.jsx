affef825859bd1a85397e33f28ef679388b789f2
Angela Devia <alegnaaived@gmail.com>
2021-09-14 21:59:40 -0500

    Replace import to SVG component

--- packages/indicators/src/app/cardManager/Card.jsx
+++ packages/indicators/src/app/cardManager/Card.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import LinkIcon from '../icons/link.svg';
+import LinkIcon from './URLIcon';
 
-const Card = ({ title, goal, period, scale, action }) => (
+const Card = ({ title, goal, period, scale, action, externalLink }) => (
   <div className="indicatorCard">
     <h1>
       {title}
       <span className="links">
-        <a className="linkURL" href={action} title="Ir al indicador">
-          <LinkIcon>..</LinkIcon>
+        <a className="linkURL" href={externalLink} title="Ir al indicador">
+          <LinkIcon fontSize={50} />
         </a>
         <a href={action} title="Abrir indicador">
           +
@@ -30,6 +30,7 @@ Card.propTypes = {
   period: PropTypes.string,
   scale: PropTypes.string,
   action: PropTypes.string.isRequired,
+  externalLink: PropTypes.string.isRequired,
 };
 
 Card.defaultProps = {
