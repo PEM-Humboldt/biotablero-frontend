35de2990d3131ae9802429072cb024f6b1964aae
Angela Devia <alegnaaived@gmail.com>
2021-09-17 13:29:52 -0500

    Reinstall rollup removed by mistake

--- packages/indicators/rollup.config.js
+++ packages/indicators/rollup.config.js
@@ -2,7 +2,6 @@ import { babel } from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import css from 'rollup-plugin-css-only';
-import reactSvg from 'rollup-plugin-react-svg';
 
 export default {
   input: 'src/App.jsx',
@@ -19,12 +18,6 @@ export default {
     babel({ exclude: 'node_modules/**', babelHelpers: 'bundled' }),
     commonjs(),
     css({ output: 'bundle.css' }),
-    reactSvg({
-      svgo: {
-        plugins: ['./src/icons/**'],
-        multipass: true,
-      },
-    }),
   ],
   external: ['react', 'react-dom'],
 };
