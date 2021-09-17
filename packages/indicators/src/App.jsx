6b772e0c23623f085e6350847788cf204d315f75
Angela Devia <alegnaaived@gmail.com>
2021-09-16 22:15:44 -0500

    Move count indicators to App

--- packages/indicators/src/App.jsx
+++ packages/indicators/src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import SaveAltIcon from '@material-ui/icons/SaveAlt';
 
 import './main.css';
 import CardManager from './app/CardManager';
@@ -20,6 +21,12 @@ const App = () => {
         </div>
       </div>
       <div>
+        <div className="countD">
+          {cardsData ? `${cardsData.length} indicadores` : 'No hay indicadores'}
+        </div>
+        <div className="countDIcon">
+          <SaveAltIcon color="#e84a5f" />
+        </div>
         <CardManager cardsData={cardsData} />
       </div>
     </div>
--- packages/indicators/src/app/CardManager.jsx
+++ packages/indicators/src/app/CardManager.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Masonry from 'react-masonry-component';
-import SaveAltIcon from '@material-ui/icons/SaveAlt';
 
 import Card from './cardManager/Card';
 
@@ -10,27 +9,19 @@ const masonryOptions = {
 };
 
 const CardManager = ({ cardsData }) => (
-  <>
-    <div className="countD">
-      {cardsData ? `${cardsData.length} indicadores` : 'No hay indicadores'}
-    </div>
-    <div className="countDIcon">
-      <SaveAltIcon color="#e84a5f" />
-    </div>
-    <Masonry options={masonryOptions}>
-      {cardsData.map((card) => (
-        <Card
-          key={card.id}
-          indicator={card.title}
-          title={card.title}
-          goal={card.goal}
-          period={card.period}
-          scale={card.scale}
-          externalLink={card.externalLink}
-        />
-      ))}
-    </Masonry>
-  </>
+  <Masonry options={masonryOptions}>
+    {cardsData.map((card) => (
+      <Card
+        key={card.id}
+        indicator={card.title}
+        title={card.title}
+        goal={card.goal}
+        period={card.period}
+        scale={card.scale}
+        externalLink={card.externalLink}
+      />
+    ))}
+  </Masonry>
 );
 
 CardManager.propTypes = {
