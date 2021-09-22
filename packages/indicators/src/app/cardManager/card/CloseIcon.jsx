e10144ce79f5b09adaccf091cfb0718d43da8bc3
Angela Devia <alegnaaived@gmail.com>
2021-09-21 22:54:17 -0500

    Create icons and update URLIcon according to Cesar SVGs

---
+++ packages/indicators/src/app/cardManager/card/CloseIcon.jsx
@@ -0,0 +1,35 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import { SvgIcon } from '@material-ui/core';
+
+const CloseIcon = ({ color, fontSize }) => (
+  <SvgIcon style={{ color, fontSize }} width="29" height="29" viewBox="0 0 29 29">
+    <g id="Ellipse_5" data-name="Ellipse 5" fill="none" stroke="#fff" strokeWidth="2">
+      <circle cx="14.5" cy="14.5" r="14.5" stroke="none" />
+      <circle cx="14.5" cy="14.5" r="13.5" fill="none" />
+    </g>
+    <line
+      id="Line_1"
+      data-name="Line 1"
+      x1="12"
+      transform="translate(8.5 14.5)"
+      fill="none"
+      stroke="#fff"
+      strokeLinecap="round"
+      strokeWidth="2"
+    />
+  </SvgIcon>
+);
+
+CloseIcon.propTypes = {
+  color: PropTypes.string,
+  fontSize: PropTypes.number,
+};
+
+CloseIcon.defaultProps = {
+  color: '',
+  fontSize: 24,
+};
+
+export default CloseIcon;
---
+++ packages/indicators/src/app/cardManager/card/OpenIcon.jsx
@@ -0,0 +1,43 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import { SvgIcon } from '@material-ui/core';
+
+const OpenIcon = ({ color, fontSize }) => (
+  <SvgIcon style={{ color, fontSize }} width="17.08" height="17.08" viewBox="0 0 17.08 17.08">
+    <g id="Group_8" data-name="Group 8" transform="translate(-743.9 -320.9)">
+      <line
+        id="Line_2"
+        data-name="Line 2"
+        x2="15.88"
+        transform="translate(744.5 329.5)"
+        fill="none"
+        stroke="#e84a60"
+        strokeLinecap="round"
+        strokeWidth="1.2"
+      />
+      <line
+        id="Line_3"
+        data-name="Line 3"
+        x2="15.88"
+        transform="translate(752.5 321.5) rotate(90)"
+        fill="none"
+        stroke="#e84a60"
+        strokeLinecap="round"
+        strokeWidth="1.2"
+      />
+    </g>
+  </SvgIcon>
+);
+
+OpenIcon.propTypes = {
+  color: PropTypes.string,
+  fontSize: PropTypes.number,
+};
+
+OpenIcon.defaultProps = {
+  color: '',
+  fontSize: 24,
+};
+
+export default OpenIcon;
