b4025637551d7a1f3337ea93c96e4a20d40258c2
Trjegul84 <angelachek@gmail.com>
2021-10-26 08:35:31 -0500

    change label name to national

--- src/pages/search/drawer/species/richness/NumberOfSpecies.jsx
+++ src/pages/search/drawer/species/richness/NumberOfSpecies.jsx
@@ -59,7 +59,8 @@ const getLabel = (key, area, region) => {
     region: `Región ${region}`,
     inferred2: 'Inferido en el área de consulta',
     observed2: 'Observado en el área de consulta',
-    areaLbl,
+    national_inferred: `Max. inferido en ${areaLbl} a nivel nacional: `,
+    national_observed: `Max. observado en ${areaLbl} a nivel nacional: `,
   }[key];
 };
 
@@ -306,7 +307,7 @@ class NumberOfSpecies extends React.Component {
                   <div>
                     {(filter === 'all' || filter === 'inferred') && (
                       <>
-                        {` Max. inferido en ${getLabel('areaLbl', areaId)} a nivel nacional: `}
+                        {getLabel('national_inferred', areaId)}
                         <b>
                           {maximumValues.find((e) => e.id === bar.id).max_inferred}
                         </b>
@@ -317,7 +318,7 @@ class NumberOfSpecies extends React.Component {
                     )}
                     {(filter === 'all' || filter === 'observed') && (
                       <>
-                        {` Max. observado en ${getLabel('areaLbl', areaId)} a nivel nacional: `}
+                        {getLabel('national_observed', areaId)}
                         <b>
                           {maximumValues.find((e) => e.id === bar.id).max_observed}
                         </b>
