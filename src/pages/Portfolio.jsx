60f26d176a954ef33fb4e960555688aa8221b922
CAScgutierrez <cgutierrez@humboldt.org.co>
2021-11-17 18:04:14 -0500

    portfolio landing

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,8 +1,81 @@
 import React from 'react';
 
 const Portfolio = () => (
-  <div>
-    Holaaaa, contenido aquí :D :D
+  <div className="wrapperPort">
+    <div className="splitPort">
+      <div className="colPort1">
+        <h1 className="portTitle">
+          Portafolios
+        </h1>
+        <div className="portText1">
+          <p>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+            sed diam nonummy nibh euismod tincidunt ut laoreet dolore
+            magna aliquam erat volutpat. Ut wisi enim ad minim veniam,
+            quis nostrud exerci tation ullamcorper suscipit lobortis
+            nisl ut aliquip ex ea commodo consequat. Duis autem vel eum
+            iriure dolor in hendrerit in vulputate velit esse molestie
+            consequat, vel illum dolore eu feugiat nulla.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+            sed diam nonummy nibh euismod tincidunt ut laoreet dolore
+            magna aliquam erat volutpat. Ut wisi enim ad minim veniam,
+            quis nostrud exerci tation ullamcorper suscipit lobortis
+            nisl ut aliquip ex ea commodo consequat. Duis autem vel eum
+            iriure dolor in hendrerit in vulputate velit esse molestie
+            consequat, vel illum dolore eu feugiat nulla.
+          </p>
+        </div>
+      </div>
+      <div className="colPort2">
+        <div className="portCard rowPort1">
+          <div className="pcTitle">
+            NATURE MAP · WCMC
+          </div>
+          <div className="pcDate">
+            2021
+          </div>
+          <p className="pcText">
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+            sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna
+            aliquam erat volutpat. Ut wisi enim ad minim veniam,
+            quis nostrud exerci tation ullamcorper suscipit
+            lobortis nisl ut aliquip ex ea commodo consequat.
+          </p>
+        </div>
+        <div className="portCard rowPort2">
+          <div className="pcTitle">
+            ELSA · PNUD
+          </div>
+          <div className="pcDate">
+            2020
+          </div>
+          <p className="pcText">
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+            sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna
+            aliquam erat volutpat. Ut wisi enim ad minim veniam,
+            quis nostrud exerci tation ullamcorper suscipit
+            lobortis nisl ut aliquip ex ea commodo consequat.
+          </p>
+        </div>
+        <div className="portCard rowPort3">
+          <div className="pcTitle">
+            WEPLAN FOREST
+          </div>
+          <div className="pcDate">
+            2021
+          </div>
+          <p className="pcText">
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit,
+            sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna
+            aliquam erat volutpat. Ut wisi enim ad minim veniam,
+            quis nostrud exerci tation ullamcorper suscipit
+            lobortis nisl ut aliquip ex ea commodo consequat.
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
