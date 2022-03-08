09080309e18bca31696d31fea17c0d27d6452e00
Angela Devia <alegnaaived@gmail.com>
2022-03-08 08:51:39 -0500

    Create portfolio description for home page

---
+++ src/pages/home/information/Portfolio.jsx
@@ -0,0 +1,93 @@
+import React from 'react';
+
+const descriptions = {
+  queEs: {
+    title: '¿Qué es un Portafolio?',
+    description: (
+      <p>
+        {'Los portafolios son un conjunto de '}
+        <b>
+          áreas priorizadas
+        </b>
+        {' para la conservación que se generan a partir de un '}
+        <b>
+          método sistemático
+        </b>
+        {' en el que se buscan soluciones costo efectivas para la conservación y gestión de la biodiversidad, seleccionando áreas donde se '}
+        <b>
+          maximiza la conservación
+        </b>
+        {' a un mínimo costo.'}
+        <br />
+        <br />
+        {' Las estrategias de conservación para las cuales se desarrollan portafolios incluyen '}
+        <b>
+          preservación, restauración y uso sostenible de la biodiversidad
+        </b>
+        .
+      </p>
+    ),
+  },
+  porque: {
+    title: '¿Por qué son importantes los Portafolios?',
+    description: (
+      <p>
+        {'Los portafolios '}
+        <b>
+          guían
+        </b>
+        {' la gestión de la biodiversidad, indicando las áreas de mayor valor para la conservación señalando las soluciones más costo efectivas, guiando así la '}
+        <b>
+          toma de decisiones
+        </b>
+        {' sobre qué y dónde conservar. La implementación de todo un portafolio en corto tiempo es poco probable, nuevos contextos surgen y la información disponible para establecer prioridades de conservación se '}
+        <b>
+          actualiza constantemente
+        </b>
+        {', lo que demanda un '}
+        <b>
+          proceso de generación de portafolios
+        </b>
+        {' dinámico y cíclico en el tiempo.'}
+      </p>
+    ),
+  },
+  quienProduce: {
+    title: '¿Quién produce los Portafolios?',
+    description: (
+      <p>
+        {'Los portafolios de conservación han sido desarrollados con base en '}
+        <b>
+          objetivos planteados en acuerdos multilaterales, políticas nacionales y regionales
+        </b>
+        {'. Los investigadores del Instituto Humboldt en alianza con diversas instituciones nacionales e internacionales, han utilizado distintas representaciones de la biodiversidad, así como diversos condicionantes para su conservación con el fin de '}
+        <b>
+          priorizar
+        </b>
+        {'  estas áreas.'}
+      </p>
+    ),
+  },
+  queEncuentras: {
+    title: '¿Qué encuentras?',
+    description: (
+      <p>
+        {'En este módulo encuentras los '}
+        <b>
+          diferentes portafolios a escala nacional
+        </b>
+        {' generados desde el Instituto Humboldt. Cada portafolio se basa en distintos objetivos y características de conservación que responden a '}
+        <b>
+          contextos de análisis particulares,
+        </b>
+        {' por esto no hay un solo portafolio posible, pues la selección de las áreas depende de los '}
+        <b>
+          objetivos de conservación
+        </b>
+        .
+      </p>
+    ),
+  },
+};
+
+export default descriptions;
