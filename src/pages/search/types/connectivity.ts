40818d6516633673fed11051bcaf952cd65d2452
Camilo Zapata <cazm013@gmail.com>
2022-08-05 15:28:03 -0500

    Create file to hold connectivity types

---
+++ src/pages/search/types/connectivity.ts
@@ -0,0 +1,17 @@
+export const currentPAConnKeys = ["prot_conn", "prot_unconn", "unprot"] as const;
+export const DPCKeys = ["muy_alto", "alto", "medio", "bajo", "muy_bajo"] as const;
+
+
+export interface currentPAConn {
+  key: typeof currentPAConnKeys[number];
+  area: number;
+  percentage: number;
+}
+
+export interface DPC {
+  id: string;
+  name: string;
+  key: typeof DPCKeys[number];
+  area: number;
+  value: number;
+}
