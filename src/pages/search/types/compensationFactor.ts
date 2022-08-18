e34589a4a374aa11896f3ba389bf423ca7ddb4a3
Camilo Zapata <cazm013@gmail.com>
2022-08-18 13:05:57 -0500

    Create fiel to hold definitions of compensation factor

---
+++ src/pages/search/types/compensationFactor.ts
@@ -0,0 +1,15 @@
+export interface biomes {
+  key: string;
+  area: number;
+};
+
+export interface cf {
+  key: number;
+  area: number;
+};
+
+export interface bioticUnits {
+  key: string;
+  area: string;
+};
+
