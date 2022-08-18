6cd5278f8385b3e0659371ab864f5534a5033437
Erika Suárez Valencia <1734998+erikasv@users.noreply.github.com>
2022-08-18 15:25:04 -0500

    Migrate LargeBarStackGraph component

--- src/components/charts/LargeBarStackGraph.tsx
+++ src/components/charts/LargeBarStackGraph.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { ResponsiveBar } from '@nivo/bar';
+import { ResponsiveBar } from "@nivo/bar";
 
-import formatNumber from 'utils/format';
+import formatNumber from "utils/format";
 
-const darkColors = {
-  '#003d59': '#003d59',
-  '#5a1d44': '#5a1d44',
-  '#902130': '#902130',
+interface Props {
+  data: Array<LargeBarStackGraphData>;
+  height?: number;
+  colors: (key: string) => string;
+  units?: string;
+  onClickGraphHandler: (key: string) => void;
+  selectedIndexValue: string;
+  labelX?: string;
+  labelY?: string;
+  padding?: number;
+}
+
+export interface LargeBarStackGraphData {
+  key: string;
+  area: number;
+  percentage: number;
+  label: string;
+}
+
+interface State {
+  selectedIndexValue: string;
+}
+
+const darkColors: { [key: string]: string } = {
+  "#003d59": "#003d59",
+  "#5a1d44": "#5a1d44",
+  "#902130": "#902130",
 };
 
-const LargeBarStackGraph = (props) => {
+const LargeBarStackGraph = (props: Props) => {
   const {
     data,
-    labelX,
-    labelY,
-    height,
+    labelX = "",
+    labelY = "",
+    height = 150,
     colors,
-    padding,
-    units,
+    padding = 0.25,
+    units = "ha",
     onClickGraphHandler,
   } = props;
 
@@ -29,15 +50,18 @@ const LargeBarStackGraph = (props) => {
    * @param {String} key id for data of each bar o serie
    * @returns {array} transformed data ready to be used by graph component
    */
-  const transformData = (rawData, key) => {
-    const transformedData = {
+  const transformData = (
+    rawData: Array<LargeBarStackGraphData>,
+    key: string
+  ) => {
+    const transformedData: Record<string, string | number> = {
       key,
     };
     rawData.forEach((item) => {
-      transformedData[String(item.key)] = Number(item.area || item.percentage);
-      transformedData[`${String(item.key)}Color`] = colors(item.key);
-      transformedData[`${String(item.key)}Label`] = item.label;
-      transformedData[`${String(item.key)}Percentage`] = Number(item.percentage);
+      transformedData[item.key] = Number(item.area || item.percentage);
+      transformedData[`${item.key}Color`] = colors(item.key);
+      transformedData[`${item.key}Label`] = item.label;
+      transformedData[`${item.key}Percentage`] = Number(item.percentage);
     });
     return [transformedData];
   };
@@ -52,38 +76,42 @@ const LargeBarStackGraph = (props) => {
    * @param {String} color color for each category inside a bar
    * @returns {object} tooltip for component
    */
-     const getToolTip = (id, allData, color) => {
-      if (allData[`${id}Percentage`]) {
-        return (
-          <div className="tooltip-graph-container">
-            <strong style={{ color: darkColors[color] ? '#ffffff' : color }}>
-              {allData[`${id}Label`]}
-            </strong>
-            <div style={{ color: '#ffffff' }}>
-              {`${formatNumber(allData[id], 0)} ${units}`}
-              <br />
-              {`${formatNumber(allData[`${id}Percentage`] * 100, 2)}%`}
-            </div>
-          </div>
-        );
-      }
+  const getToolTip = (
+    id: string | number,
+    allData: Record<string, string | number>,
+    color: string
+  ) => {
+    if (allData[`${id}Percentage`]) {
       return (
         <div className="tooltip-graph-container">
-          <strong style={{ color: darkColors[color] ? '#ffffff' : color }}>
+          <strong style={{ color: darkColors[color] ? "#ffffff" : color }}>
             {allData[`${id}Label`]}
           </strong>
-          <div style={{ color: '#ffffff' }}>
+          <div style={{ color: "#ffffff" }}>
             {`${formatNumber(allData[id], 0)} ${units}`}
+            <br />
+            {`${formatNumber(Number(allData[`${id}Percentage`]) * 100, 2)}%`}
           </div>
         </div>
       );
-    };
+    }
+    return (
+      <div className="tooltip-graph-container">
+        <strong style={{ color: darkColors[color] ? "#ffffff" : color }}>
+          {allData[`${id}Label`]}
+        </strong>
+        <div style={{ color: "#ffffff" }}>
+          {`${formatNumber(allData[id], 0)} ${units}`}
+        </div>
+      </div>
+    );
+  };
 
   return (
     <div style={{ height }}>
       <ResponsiveBar
         data={transformData(data, labelY)}
-        onClick={(category) => onClickGraphHandler(category.id)}
+        onClick={(category) => onClickGraphHandler(String(category.id))}
         keys={keys}
         indexBy="key"
         layout="horizontal"
@@ -94,59 +122,34 @@ const LargeBarStackGraph = (props) => {
           left: 40,
         }}
         padding={padding}
-        colors={(obj) => colors(obj.id)}
+        colors={(obj) => colors(String(obj.id))}
         enableGridX
         borderWidth={0}
-        borderColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
+        borderColor={{ from: "color", modifiers: [["darker", 1.6]] }}
         axisLeft={null}
         axisBottom={{
           tickSize: 0,
           tickPadding: 0,
           tickRotation: 0,
-          format: '.2s',
+          format: ".2s",
           legend: labelX,
-          legendPosition: 'start',
+          legendPosition: "start",
           legendOffset: 25,
         }}
         enableLabel={false}
         labelSkipWidth={12}
         labelSkipHeight={12}
-        labelTextColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
+        labelTextColor={{ from: "color", modifiers: [["darker", 1.6]] }}
         animate
-        motionStiffness={90}
-        motionDamping={15}
-        tooltip={({ id, data: allData, color }) => getToolTip(id, allData, color)}
+        tooltip={({ id, data: allData, color }) =>
+          getToolTip(id, allData, color)
+        }
         theme={{
-          axis: { legend: { text: { fontSize: '14' } } },
+          axis: { legend: { text: { fontSize: "14" } } },
         }}
       />
     </div>
   );
-};
-
-LargeBarStackGraph.propTypes = {
-  data: PropTypes.arrayOf(PropTypes.shape({
-    key: PropTypes.string.isRequired,
-    area: PropTypes.number.isRequired,
-    percentage: PropTypes.number,
-    label: PropTypes.string.isRequired,
-  })).isRequired,
-  labelX: PropTypes.string,
-  labelY: PropTypes.string,
-  height: PropTypes.number,
-  colors: PropTypes.func,
-  padding: PropTypes.number,
-  units: PropTypes.string,
-  onClickGraphHandler: PropTypes.func.isRequired,
-};
-
-LargeBarStackGraph.defaultProps = {
-  labelX: '',
-  labelY: '',
-  height: 150,
-  colors: () => {},
-  padding: 0.25,
-  units: 'ha',
 };
 
 export default LargeBarStackGraph;
