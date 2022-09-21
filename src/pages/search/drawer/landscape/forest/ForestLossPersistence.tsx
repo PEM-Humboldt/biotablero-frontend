7e6e6d8919410c91850b57d573ddb1b6969e1d18
Trjegul84 <angelachek@gmail.com>
2022-09-21 16:32:08 -0500

    change imports types and var names

--- src/pages/search/drawer/landscape/forest/ForestLossPersistence.tsx
+++ src/pages/search/drawer/landscape/forest/ForestLossPersistence.tsx
@@ -9,9 +9,9 @@ import SearchAPI from "utils/searchAPI";
 import formatNumber from "utils/format";
 import TextBoxes from "pages/search/shared_components/TextBoxes";
 
-import MultiSmallBarStackGraph, {
-  MultiSmallBarStackGraphData,
-} from "pages/search/shared_components/charts/MultiSmallBarStackGraph";
+import MultiSmallStackedBar, {
+  MultiSmallStackedBarData,
+} from "pages/search/shared_components/charts/MultiSmallStackedBar";
 import { textsObject } from "pages/search/types/texts";
 import { ForestLP } from "pages/search/types/forest";
 import { wrapperMessage } from "pages/search/types/charts";
@@ -19,7 +19,7 @@ import { wrapperMessage } from "pages/search/types/charts";
 interface Props {}
 interface State {
   showInfoGraph: boolean;
-  forestLP: Array<MultiSmallBarStackGraphData>;
+  forestLP: Array<MultiSmallStackedBarData>;
   message: wrapperMessage;
   forestPersistenceValue: number;
   texts: {
@@ -116,7 +116,7 @@ class ForestLossPersistence extends React.Component<Props, State> {
    *
    * @param {Array<Object>} data graph data transformed to be downloaded as csv
    */
-  processDownload = (data: Array<MultiSmallBarStackGraphData>) => {
+  processDownload = (data: Array<MultiSmallStackedBarData>) => {
     const result: Array<{
       period: string;
       category: string;
@@ -170,7 +170,7 @@ class ForestLossPersistence extends React.Component<Props, State> {
           <h6>Cobertura de bosque en el tiempo</h6>
         </div>
         <div>
-          <MultiSmallBarStackGraph
+          <MultiSmallStackedBar
             data={forestLP}
             message={message}
             units="ha"
