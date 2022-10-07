cc722a628b1d4b610ded74da44ee3e6e363cb857
Manuel Gálvez <mgalvez@humboldt.org.co>
2022-10-06 20:45:08 -0500

    Update import component name and properties from SmallStackedBars to MultiSmallBars

--- src/pages/search/drawer/landscape/forest/ForestLossPersistence.tsx
+++ src/pages/search/drawer/landscape/forest/ForestLossPersistence.tsx
@@ -9,9 +9,9 @@ import SearchAPI from "utils/searchAPI";
 import formatNumber from "utils/format";
 import TextBoxes from "pages/search/shared_components/TextBoxes";
 
-import SmallStackedBars, {
-  SmallStackedBarsData,
-} from "pages/search/shared_components/charts/SmallStackedBars";
+import MultiSmallBars, {
+  MultiSmallBarsData,
+} from "pages/search/shared_components/charts/MultiSmallBars";
 import { textsObject } from "pages/search/types/texts";
 import { wrapperMessage } from "pages/search/types/charts";
 import { ForestLossPersistenceController } from "pages/search/drawer/landscape/forest/ForestLossPersistenceController";
@@ -19,7 +19,7 @@ import { ForestLossPersistenceController } from "pages/search/drawer/landscape/f
 interface Props {}
 interface State {
   showInfoGraph: boolean;
-  forestLP: Array<SmallStackedBarsData>;
+  forestLP: Array<MultiSmallBarsData>;
   message: wrapperMessage;
   forestPersistenceValue: number;
   texts: {
@@ -141,7 +141,7 @@ class ForestLossPersistence extends React.Component<Props, State> {
           <h6>Cobertura de bosque en el tiempo</h6>
         </div>
         <div>
-          <SmallStackedBars
+          <MultiSmallBars
             data={forestLP}
             message={message}
             units="ha"
@@ -154,6 +154,8 @@ class ForestLossPersistence extends React.Component<Props, State> {
               });
             }}
             selectedIndexValue="2016-2021"
+            axisLeftLegend="Periodo"
+            axisBottomLegend="Hectáreas"
           />
         </div>
         <TextBoxes
