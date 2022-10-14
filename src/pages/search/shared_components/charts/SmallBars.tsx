ba34c08fcb4972a8430dacfd5d6f2e29258fbf62
Manuel Gálvez <mgalvez@humboldt.org.co>
2022-10-14 14:14:49 -0500

    Merge MultiSmallBars into SmallBars

--- src/pages/search/shared_components/charts/SmallBars.tsx
+++ src/pages/search/shared_components/charts/SmallBars.tsx
@@ -1,25 +1,19 @@
 import React from "react";
-import { ResponsiveBar } from "@nivo/bar";
+import { ResponsiveBar, BarSvgProps, BarDatum } from "@nivo/bar";
 import { darkenColor } from "utils/colorUtils";
 import formatNumber from "utils/format";
 import withMessageWrapper from "pages/search/shared_components/charts/withMessageWrapper";
 
-interface Props {
-  data: Array<SmallBarsData>;
+interface Props extends Partial<Omit<BarSvgProps<BarDatum>, "colors">> {
+  data: Array<BarDatum>;
   height?: number;
   colors: (key: string) => string;
-  units?: string;
-  onClickHandler: (key: string) => void;
+  onClickHandler: (
+    period?: string,
+    key?: string,
+    selected?: string | undefined
+  ) => void;
   selectedIndexValue?: string;
-  labelX: string;
-}
-
-export interface SmallBarsData {
-  id: string;
-  name: string;
-  key: string;
-  value: number;
-  area: number;
 }
 
 interface State {
@@ -38,97 +32,62 @@ class SmallBars extends React.Component<Props, State> {
   render() {
     const {
       data,
-      height = 250,
+      keys,
+      indexBy = "id",
+      margin,
+      padding,
+      height = 500,
       colors,
-      units = "ha",
+      layout = "horizontal",
+      groupMode = "stacked",
+      axisTop = null,
+      axisRight = null,
+      axisBottom = null,
+      axisLeft = null,
+      enableLabel = false,
+      enableGridX = false,
+      enableGridY = false,
       onClickHandler,
-      labelX,
+      tooltip = undefined,
     } = this.props;
     const { selectedIndexValue } = this.state;
-
-    const transformData = (rawData: Array<SmallBarsData>) => {
-      const transformedData = rawData.map((element) => {
-        const object: Record<string, string | number> = {
-          id: String(element.id),
-        };
-        object[String(element.id)] = Number(element.value);
-        object[`${String(element.id)}Label`] = element.name;
-        object[`${String(element.id)}Color`] = colors(element.key);
-        object[`${String(element.id)}DarkenColor`] = darkenColor(
-          colors(element.key),
-          15
-        );
-        object[`${String(element.id)}Area`] = Number(element.area);
-        return object;
-      });
-      return transformedData;
-    };
-
-    /**
-     * Get keys to be passed to component as a prop
-     *
-     * @returns {array} ids of each bar category removing duplicates
-     */
-    const keys = data ? [...new Set(data.map((item) => String(item.id)))] : [];
 
     return (
       <div style={{ height }}>
         <ResponsiveBar
-          data={transformData(data)}
+          data={data}
           keys={keys}
-          indexBy="id"
-          layout="horizontal"
-          margin={{
-            top: 20,
-            right: 15,
-            bottom: 50,
-            left: 40,
-          }}
-          padding={0.35}
-          borderColor={{ from: "color", modifiers: [["darker", 1.6]] }}
-          colors={({ id, indexValue, data: allData }) => {
-            if (indexValue === selectedIndexValue) {
-              return String(allData[`${id}DarkenColor`]);
-            }
-            return String(allData[`${id}Color`]);
-          }}
-          enableGridY={false}
-          enableGridX
-          axisLeft={null}
-          axisBottom={{
-            tickSize: 0,
-            tickPadding: 0,
-            tickRotation: 0,
-            format: ".2f",
-            legend: labelX,
-            legendPosition: "start",
-            legendOffset: 25,
-          }}
-          enableLabel
+          margin={margin}
+          indexBy={indexBy}
+          layout={layout}
+          groupMode={groupMode}
+          padding={padding}
+          axisTop={axisTop}
+          axisRight={axisRight}
+          axisBottom={axisBottom}
+          axisLeft={axisLeft}
+          enableLabel={enableLabel}
+          enableGridX={enableGridX}
+          enableGridY={enableGridY}
           label={({ value }) => (value ? formatNumber(value, 2) : "")}
           animate
-          tooltip={({ id, data: allData, color }) => (
-            <div
-              className="tooltip-graph-container"
-              style={{ position: "absolute" }}
-            >
-              <strong style={{ color }}>{allData[`${id}Label`]}</strong>
-              <div style={{ color: "#ffffff" }}>
-                {formatNumber(allData[id], 2)}
-                <br />
-                {`${formatNumber(allData[`${id}Area`], 2)} ${units}`}
-              </div>
-            </div>
-          )}
           theme={{
             axis: {
               legend: { text: { fontSize: "14" } },
             },
           }}
+          colors={({ id, indexValue }) => {
+            if (indexValue === selectedIndexValue) {
+              return darkenColor(colors(String(id)), 15);
+            }
+            return colors(String(id));
+          }}
+          borderColor={{ from: "color", modifiers: [["darker", 1.6]] }}
           onClick={({ indexValue }) => {
             this.setState({ selectedIndexValue: String(indexValue) });
             onClickHandler(String(indexValue));
           }}
+          tooltip={tooltip}
         />
       </div>
     );
