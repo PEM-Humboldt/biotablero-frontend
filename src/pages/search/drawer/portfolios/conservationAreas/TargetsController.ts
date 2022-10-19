dd8d8bb18a215bea86fa54d03d70ead4fded257c
Manuel Gálvez <mgalvez@humboldt.org.co>
2022-10-19 16:01:36 -0500

    Update Transformation Data Function

--- src/pages/search/drawer/portfolios/conservationAreas/TargetsController.ts
+++ src/pages/search/drawer/portfolios/conservationAreas/TargetsController.ts
@@ -1,8 +1,9 @@
-import { MultiSmallBarsData } from "pages/search/shared_components/charts/MultiSmallBars";
+import { SmallBarsData } from "pages/search/shared_components/charts/SmallBars";
 import {
   portfoliosByTarget,
   portfolioData,
 } from "pages/search/types/portfolios";
+import formatNumber from "utils/format";
 
 export class TargetsController {
   constructor() {}
@@ -12,35 +13,46 @@ export class TargetsController {
    *
    * @param {Array<portfoliosByTarget>} rawData raw data from RestAPI
    *
-   * @returns {Array<MultiSmallBarsData>} transformed data ready to be used by graph component
+   * @returns {Array<SmallBarsData>} transformed data ready to be used by graph component
    */
   getGraphData(rawData: Array<portfoliosByTarget>) {
-    const transformedData: Array<MultiSmallBarsData> = rawData.map(
-      (element) => {
-        const objectData: Array<{
-          area: number;
-          key: string;
-          percentage: number;
-          label: string;
-        }> = [];
-        element.portfolios_data.forEach((item: portfolioData) => {
-          const info = {
-            key: item.short_name,
-            area: item.value,
-            label: item.name,
-            percentage: (item.value / element.target_national) * 100,
-          };
-          objectData.push(info);
-        });
-
-        const object = {
-          id: element.target_name,
-          data: objectData,
+    const tooltips: Array<{
+      group: string;
+      category: string;
+      tooltipContent: Array<string>;
+    }> = [];
+    const transformedData: Array<SmallBarsData> = rawData.map((element) => {
+      const objectData: Array<{ category: string; value: number }> = [];
+      element.portfolios_data.forEach((item: portfolioData) => {
+        const percentage = (item.value / element.target_national) * 100;
+        const info = {
+          category: item.short_name,
+          value: percentage,
         };
-        return object;
-      }
-    );
+        objectData.push(info);
 
-    return transformedData;
+        tooltips.push({
+          group: element.target_name,
+          category: item.short_name,
+          tooltipContent: [
+            item.name,
+            `${formatNumber(percentage, 2)} %`,
+            `${formatNumber(item.value, 2)} c`,
+          ],
+        });
+      });
+
+      const object = {
+        group: element.target_name,
+        data: objectData,
+      };
+      return object;
+    });
+
+    const keys = rawData[0]
+      ? rawData[0].portfolios_data.map((item) => String(item.short_name))
+      : [];
+
+    return { transformedData, keys, tooltips };
   }
 }
