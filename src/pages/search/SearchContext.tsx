5371dcc257845b6e84167cd7ef823811ac264c3a
Manuel Gálvez <mgalvez@humboldt.org.co>
2022-11-15 09:09:16 -0500

    Define searchType in SearchContext

--- src/pages/search/SearchContext.tsx
+++ src/pages/search/SearchContext.tsx
@@ -3,6 +3,7 @@ import React from "react";
 export interface SearchContextValues {
   areaId: string;
   geofenceId: string | number;
+  searchType: "selection" | "polygon";
   switchLayer(layer: string): void;
   handlerClickOnGraph({}): void;
   cancelActiveRequests(): void;
@@ -11,6 +12,7 @@ export interface SearchContextValues {
 const SearchContext = React.createContext<SearchContextValues>({
   areaId: "",
   geofenceId: "",
+  searchType: "selection",
   switchLayer: () => {},
   handlerClickOnGraph: () => {},
   cancelActiveRequests: () => {},
