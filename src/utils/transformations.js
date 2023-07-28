64aadea959b8bc4315d6d0b5499b0f2d772e5b17
Camilo Zapata <cazm013@gmail.com>
2023-07-27 22:13:09 -0500

    Fix function toMultipolygonWKT to avoid change the order of the coordinates of the original polygon object

--- src/utils/transformations.js
+++ src/utils/transformations.js
@@ -4,11 +4,19 @@
  */
 
 const toMultipolygonWKT = (polygon) => {
-    if(polygon && polygon.coordinates) {
-      const coordinates = polygon.coordinates.map(p => p.reverse());
-      coordinates.push(coordinates[0]);
-      return "MULTIPOLYGON (((" + coordinates.map((coord) => coord.join(" ")).join(",") + ")))";
-    }
+  if (polygon && polygon.coordinates) {
+    const coordinates = [];
+    polygon.coordinates.forEach((p) => {
+      const reversedCoord = [p[1], p[0]];
+      coordinates.push(reversedCoord);
+    });
+    coordinates.push(coordinates[0]);
+    return (
+      "MULTIPOLYGON (((" +
+      coordinates.map((coord) => coord.join(" ")).join(",") +
+      ")))"
+    );
   }
+};
 
 export { toMultipolygonWKT };
