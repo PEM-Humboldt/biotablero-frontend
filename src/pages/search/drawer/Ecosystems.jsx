e90cfb60c22daad7d90d86e6a09ac1d6c68fb359
hugoTrujillo <hugoalbertotrujillo@gmail.com>
2023-08-31 01:14:38 -0500

    Fix style in message

--- src/pages/search/drawer/Ecosystems.jsx
+++ src/pages/search/drawer/Ecosystems.jsx
@@ -380,7 +380,7 @@ class Ecosystems extends React.Component {
           <div className="ecoest">
             <h4 className="minus20">
               Ecosistemas estratégicos
-              <b>{`${formatNumber(SETotalArea, 0)} ha` }</b>
+              <b>{`${formatNumber(SETotalArea, 0)} ha`}</b>
             </h4>
             <IconTooltip title="Interpretación">
               <InfoIcon
@@ -389,13 +389,13 @@ class Ecosystems extends React.Component {
               />
             </IconTooltip>
             <h5 className="minusperc">
-            {`${getPercentage(SETotalArea, generalArea)} %`}
+              {`${getPercentage(SETotalArea, generalArea)} %`}
             </h5>
-            <h5 className="minusperc">
-            {getPercentage(SETotalArea, generalArea) > 100 ? ` La superposición de ecosistemas estratégicos puede resultar en que su valor total exceda el área de consulta, al contar múltiples veces zonas donde coexisten.` : `` }
-              
 
-            </h5>
+            <h2 className="minusperc">
+              {getPercentage(SETotalArea, generalArea) > 100 ? 'La superposición de ecosistemas estratégicos puede resultar en que su valor total exceda el área de consulta, al contar múltiples veces zonas donde coexisten.' : '' }
+            </h2>
+
             {infoShown.has('se') && (
               <ShortInfo
                 description={`<p>${texts.se.info}</p>`}
