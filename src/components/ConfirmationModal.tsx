9fbffdefcd3542f9339c5c04cffac7d5a1b08898
hache <hugoalbertotrujillo@gmail.com>
2023-11-15 18:12:03 -0500

    Add ConfirmationModal.tsx file

---
+++ src/components/ConfirmationModal.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Modal from "@mui/material/Modal";
+
+interface ConfirmationModalTypes {
+  open: boolean;
+  onClose: () => void;
+  message: string;
+  styleCustom: string | null;
+  onContinue: () => void;
+  onCancel: () => void;
+}
+
+const ConfirmationModal: React.FC<ConfirmationModalTypes> = ({
+  open,
+  onClose,
+  message,
+  onContinue,
+  onCancel,
+  styleCustom,
+}) => (
+  <Modal
+    aria-labelledby="simple-modal-title"
+    aria-describedby="simple-modal-description"
+    open={open}
+    onClose={onClose}
+  >
+    <div className={styleCustom || "newBiomeAlarm"}>
+      <div>{message}</div>
+      <button type="button" onClick={onContinue}>
+        Si
+      </button>
+      <button type="button" onClick={onCancel}>
+        No
+      </button>
+    </div>
+  </Modal>
+);
+
+export default ConfirmationModal;
