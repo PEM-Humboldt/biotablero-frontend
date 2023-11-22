321e08d710c11ff7c1be31be4e76aa286f1c86d9
hache <hugoalbertotrujillo@gmail.com>
2023-11-21 19:20:15 -0500

    Add typescript to component MenuButton

---
+++ src/pages/home/content/MenuButton.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+interface MenuButtonTypes {
+  buttonStyles: string;
+  idBtn: string;
+  focusCallback: () => void;
+  firstLineContent: string;
+  secondLineContent: string;
+  externalLink: string;
+  localLink: string;
+}
+
+const MenuButton: React.FC<MenuButtonTypes> = ({
+  buttonStyles,
+  idBtn,
+  focusCallback,
+  firstLineContent,
+  secondLineContent,
+  externalLink,
+  localLink,
+}) => {
+  const content = (
+    <button
+      type="button"
+      className={buttonStyles}
+      id={idBtn}
+      onMouseOver={focusCallback}
+      onFocus={focusCallback}
+    >
+      {`${firstLineContent} `}
+      <b>{secondLineContent}</b>
+    </button>
+  );
+
+  if (externalLink) {
+    return (
+      <a href={externalLink} target="_blank" rel="noopener noreferrer">
+        {content}
+      </a>
+    );
+  }
+  return <Link to={localLink}>{content}</Link>;
+};
+
+export default MenuButton;
