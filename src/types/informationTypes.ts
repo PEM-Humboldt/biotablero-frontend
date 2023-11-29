93b8b9eb6fd4cbf4cc8546469651dfd33a36f6dc
hache <hugoalbertotrujillo@gmail.com>
2023-11-28 20:30:52 -0500

    Add typescript to portfolio and information directories

---
+++ src/types/informationTypes.ts
@@ -0,0 +1,11 @@
+interface descriptionsTypes {
+  title: string;
+  description: JSX.Element;
+}
+
+export interface BasicTitleTypes {
+  queEs: descriptionsTypes;
+  porque: descriptionsTypes;
+  quienProduce: descriptionsTypes;
+  queEncuentras: descriptionsTypes;
+}
