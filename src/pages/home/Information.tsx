53191521bbeb0a6284cb2fa21e44fac15612310c
hache <hugoalbertotrujillo@gmail.com>
2023-11-30 00:26:16 -0500

    Add prettier

--- src/pages/home/Information.tsx
+++ src/pages/home/Information.tsx
@@ -1,12 +1,11 @@
-import React, { useState } from 'react';
-//import PropTypes from 'prop-types';
+import React, { useState } from "react";
 
-import AlertDescriptions from 'pages/home/information/Alerts';
-import CompensationDescriptions from 'pages/home/information/Compensations';
-import IndicatorDescriptions from 'pages/home/information/Indicators';
-import PortfolioDescriptions from 'pages/home/information/Portfolio';
-import SearchDescriptions from 'pages/home/information/Searches';
-import CbmdDescriptions from 'pages/home/information/Cbmd';
+import AlertDescriptions from "pages/home/information/Alerts";
+import CompensationDescriptions from "pages/home/information/Compensations";
+import IndicatorDescriptions from "pages/home/information/Indicators";
+import PortfolioDescriptions from "pages/home/information/Portfolio";
+import SearchDescriptions from "pages/home/information/Searches";
+import CbmdDescriptions from "pages/home/information/Cbmd";
 
 import { BasicTitleTypes } from "types/informationTypes";
 
@@ -14,16 +13,16 @@ interface InformationTypes {
   activeModule: string;
 }
 
-const Information: React.FC<InformationTypes> = ({activeModule})  => {
-  const [activeItem, setActiveItem] = useState<string>('queEs');
+const Information: React.FC<InformationTypes> = ({ activeModule }) => {
+  const [activeItem, setActiveItem] = useState<string>("queEs");
 
   interface ContentInfoTypes {
-    search:  BasicTitleTypes;
-    indicator:  BasicTitleTypes;
-    portfolio:  BasicTitleTypes;
-    compensation:  BasicTitleTypes;
-    alert:  BasicTitleTypes;
-    cbmdashboard:  BasicTitleTypes;
+    search: BasicTitleTypes;
+    indicator: BasicTitleTypes;
+    portfolio: BasicTitleTypes;
+    compensation: BasicTitleTypes;
+    alert: BasicTitleTypes;
+    cbmdashboard: BasicTitleTypes;
   }
 
   const contentInfo: any = {
@@ -35,152 +34,58 @@ const Information: React.FC<InformationTypes> = ({activeModule})  => {
     cbmdashboard: CbmdDescriptions,
   };
 
-  const { title, description } = contentInfo[activeModule] ? contentInfo[activeModule][activeItem] : { title: '', description: '' };
+  const { title, description } = contentInfo[activeModule]
+    ? contentInfo[activeModule][activeItem]
+    : { title: "", description: "" };
 
   return (
     <div className="menuline">
       <menu>
         <button
           type="button"
-          className={`btnhome btn1 ${(activeItem === 'queEs') ? 'active' : ''}`}
-          onClick={() => setActiveItem( 'queEs' )}
+          className={`btnhome btn1 ${activeItem === "queEs" ? "active" : ""}`}
+          onClick={() => setActiveItem("queEs")}
         >
-          <b>
-            01
-          </b>
-          {' ¿Qué es?'}
+          <b>01</b>
+          {" ¿Qué es?"}
         </button>
         <button
           type="button"
-          className={`btnhome btn2 ${(activeItem === 'porque') ? 'active' : ''}`}
-          onClick={() => setActiveItem('porque' )}
+          className={`btnhome btn2 ${activeItem === "porque" ? "active" : ""}`}
+          onClick={() => setActiveItem("porque")}
         >
-          <b>
-            02
-          </b>
-          {' ¿Por qué?'}
+          <b>02</b>
+          {" ¿Por qué?"}
         </button>
         <button
           type="button"
-          className={`btnhome btn3 ${(activeItem === 'quienProduce') ? 'active' : ''}`}
-          onClick={() => setActiveItem('quienProduce' )}
+          className={`btnhome btn3 ${
+            activeItem === "quienProduce" ? "active" : ""
+          }`}
+          onClick={() => setActiveItem("quienProduce")}
         >
-          <b>
-            03
-          </b>
-          {' ¿Quién produce?'}
+          <b>03</b>
+          {" ¿Quién produce?"}
         </button>
         <button
           type="button"
-          className={`btnhome btn4 ${(activeItem === 'queEncuentras') ? 'active' : ''}`}
-          onClick={() => setActiveItem('queEncuentras' )}
+          className={`btnhome btn4 ${
+            activeItem === "queEncuentras" ? "active" : ""
+          }`}
+          onClick={() => setActiveItem("queEncuentras")}
         >
-          <b>
-            04
-          </b>
-          {' ¿Qué encuentras?'}
+          <b>04</b>
+          {" ¿Qué encuentras?"}
         </button>
       </menu>
       <div className={`${activeModule}`}>
         <div className={`content ${activeItem}`}>
-          <h1>
-            {title}
-          </h1>
+          <h1>{title}</h1>
           {description}
         </div>
       </div>
     </div>
   );
-
-}
-
-// class Information2 extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       activeItem: 'queEs',
-//     };
-//     this.contentInfo = {
-//       search: SearchDescriptions,
-//       indicator: IndicatorDescriptions,
-//       portfolio: PortfolioDescriptions,
-//       compensation: CompensationDescriptions,
-//       alert: AlertDescriptions,
-//       cbmdashboard: CbmdDescriptions,
-//     };
-//   }
-
-//   static getDerivedStateFromProps(nextProps) {
-//     return { activeModule: nextProps.activeModule };
-//   }
-
-//   render() {
-//     const { activeModule } = this.props;
-//     const { activeItem } = this.state;
-//     const { title, description } = this.contentInfo[activeModule] ? this.contentInfo[activeModule][activeItem] : { title: 'HOLI', description: 'HACHE' };
-//     return (
-//       <div className="menuline">
-//         <menu>
-//           <button
-//             type="button"
-//             className={`btnhome btn1 ${(activeItem === 'queEs') ? 'active' : ''}`}
-//             onClick={() => this.setState({ activeItem: 'queEs' })}
-//           >
-//             <b>
-//               01
-//             </b>
-//             {' ¿Qué es?'}
-//           </button>
-//           <button
-//             type="button"
-//             className={`btnhome btn2 ${(activeItem === 'porque') ? 'active' : ''}`}
-//             onClick={() => this.setState({ activeItem: 'porque' })}
-//           >
-//             <b>
-//               02
-//             </b>
-//             {' ¿Por qué?'}
-//           </button>
-//           <button
-//             type="button"
-//             className={`btnhome btn3 ${(activeItem === 'quienProduce') ? 'active' : ''}`}
-//             onClick={() => this.setState({ activeItem: 'quienProduce' })}
-//           >
-//             <b>
-//               03
-//             </b>
-//             {' ¿Quién produce?'}
-//           </button>
-//           <button
-//             type="button"
-//             className={`btnhome btn4 ${(activeItem === 'queEncuentras') ? 'active' : ''}`}
-//             onClick={() => this.setState({ activeItem: 'queEncuentras' })}
-//           >
-//             <b>
-//               04
-//             </b>
-//             {' ¿Qué encuentras?'}
-//           </button>
-//         </menu>
-//         <div className={`${activeModule}`}>
-//           <div className={`content ${activeItem}`}>
-//             <h1>
-//               {title}
-//             </h1>
-//             {description}
-//           </div>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-// Information.propTypes = {
-//   activeModule: PropTypes.string,
-// };
-
-// Information.defaultProps = {
-//   activeModule: '',
-// };
+};
 
 export default Information;
