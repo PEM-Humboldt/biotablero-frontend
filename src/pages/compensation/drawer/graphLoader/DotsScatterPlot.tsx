a5605b84bdae29503d9abd2504104bb2d97d54e4
Hugo Trujillo <htrujillo@humboldt.org.co>
2024-01-20 02:40:02 -0500

    Order code and write variables correctly

--- src/pages/compensation/drawer/graphLoader/DotsScatterPlot.tsx
+++ src/pages/compensation/drawer/graphLoader/DotsScatterPlot.tsx
@@ -1,219 +1,5 @@
 import { ResponsiveScatterPlot } from "@nivo/scatterplot";
 
-import { AriaAttributes, FunctionComponent, MouseEvent } from "react";
-import { SpringValues } from "@react-spring/web";
-import {
-  Dimensions,
-  Box,
-  Theme,
-  ValueFormat,
-  MotionProps,
-  CssMixBlendMode,
-  CartesianMarkerProps,
-  PropertyAccessor,
-} from "@nivo/core";
-import { AnyScale, ScaleSpec } from "@nivo/scales";
-import { OrdinalColorScaleConfig } from "@nivo/colors";
-import { AxisProps, GridValues } from "@nivo/axes";
-import { LegendProps } from "@nivo/legends";
-import { AnnotationMatcher } from "@nivo/annotations";
-
-/* interface DataTypes {
-    x: string;
-    y: string;
-    z: string;
-    biome: string;
-}
-
-interface DataListTypes {
-    color: string;
-    data: Array<DataTypes>
-    formattedX: string
-    formattedY: string
-    id: string
-    index: number
-    serieId: string
-    serieIndex: number
-    size: number
-    x: number
-    xValue: number
-    y: number
-    yValue: number
-} */
-
-/**
- * Typescript encontrado en el repo de NIVO
- * https://github.com/plouc/nivo/blob/d87af09a2287f55d0496d007ff995c5fa8fc3de2/packages/scatterplot/src/types.ts#L22
- */
-//////////////////////////////////////////////////////////////////////
-export type ScatterPlotValue = string;
-
-export interface ScatterPlotDatum {
-  x: ScatterPlotValue;
-  y: ScatterPlotValue;
-  z: ScatterPlotValue;
-  biome: ScatterPlotValue;
-}
-
-export type ScatterPlotRawSerie<RawDatum extends ScatterPlotDatum> = {
-  id: string | number;
-  data: RawDatum[];
-};
-
-export interface ScatterPlotNodeData<RawDatum extends ScatterPlotDatum> {
-  // absolute index, relative to all points in all series
-  index: number;
-  // relative index, in a specific serie
-  serieIndex: number;
-  id: string;
-  serieId: ScatterPlotRawSerie<RawDatum>["id"];
-  // x position
-  x: number;
-  xValue: RawDatum["x"];
-  formattedX: string | number;
-  // y position
-  y: number;
-  yValue: RawDatum["y"];
-  formattedY: string | number;
-  size: number;
-  color: string;
-  data: RawDatum;
-}
-
-//////////////////////////////////////////////////////////////////////////////////
-/* export interface ScatterPlotNodeProps<RawDatum extends ScatterPlotDatum> {
-    node: ScatterPlotNodeData<RawDatum>
-    style: SpringValues<{
-        x: number
-        y: number
-        size: number
-        color: string
-    }>
-    blendMode: CssMixBlendMode
-    isInteractive: boolean
-    onMouseEnter?: ScatterPlotMouseHandler<RawDatum>
-    onMouseMove?: ScatterPlotMouseHandler<RawDatum>
-    onMouseLeave?: ScatterPlotMouseHandler<RawDatum>
-    onClick?: ScatterPlotMouseHandler<RawDatum>
-} 
-export type ScatterPlotNode<RawDatum extends ScatterPlotDatum> = FunctionComponent<
-    ScatterPlotNodeProps<RawDatum>
-> */
-//////////////////////////////////////////////////////////////////////////
-export interface ScatterPlotTooltipProps<RawDatum extends ScatterPlotDatum> {
-  node: ScatterPlotNodeData<RawDatum>;
-}
-//////////////////////////////////////////////////////////////////////
-/* export type ScatterPlotTooltip<RawDatum extends ScatterPlotDatum> = FunctionComponent<
-    ScatterPlotTooltipProps<RawDatum>
-> */
-
-/* export type ScatterPlotLayerId =
-    | 'grid'
-    | 'axes'
-    | 'nodes'
-    | 'markers'
-    | 'mesh'
-    | 'legends'
-    | 'annotations'
-export interface ScatterPlotLayerProps<RawDatum extends ScatterPlotDatum> {
-    xScale: AnyScale
-    yScale: AnyScale
-    nodes: ScatterPlotNodeData<RawDatum>[]
-    innerWidth: number
-    innerHeight: number
-    outerWidth: number
-    outerHeight: number
-}
-export type ScatterPlotCustomSvgLayer<RawDatum extends ScatterPlotDatum> = FunctionComponent<
-    ScatterPlotLayerProps<RawDatum>
->
-export type ScatterPlotCustomCanvasLayer<RawDatum extends ScatterPlotDatum> = (
-    ctx: CanvasRenderingContext2D,
-    props: ScatterPlotLayerProps<RawDatum>
-) => void
-
-export interface ScatterPlotNodeDynamicSizeSpec {
-    key: string
-    values: [number, number]
-    sizes: [number, number]
-}
-
-export type ScatterPlotMouseHandler<RawDatum extends ScatterPlotDatum> = (
-    node: ScatterPlotNodeData<RawDatum>,
-    event: MouseEvent<any>
-) => void
-//
-export interface ScatterPlotDataProps<RawDatum extends ScatterPlotDatum> {
-    data: ScatterPlotRawSerie<RawDatum>[]
-}
-
-export type ScatterPlotCommonProps<RawDatum extends ScatterPlotDatum> = {
-    nodeId: PropertyAccessor<Omit<ScatterPlotNodeData<RawDatum>, 'id' | 'size' | 'color'>, string>
-    xScale: ScaleSpec
-    xFormat: ValueFormat<RawDatum['x']>
-    yScale: ScaleSpec
-    yFormat: ValueFormat<RawDatum['y']>
-    margin: Box
-    enableGridX: boolean
-    gridXValues: GridValues<RawDatum['x']>
-    enableGridY: boolean
-    gridYValues: GridValues<RawDatum['y']>
-    axisTop: AxisProps | null
-    axisRight: AxisProps | null
-    axisBottom: AxisProps | null
-    axisLeft: AxisProps | null
-    theme: Theme
-    //OrdinalColorScaleConfigCustomFunction
-    colors: OrdinalColorScaleConfig<{ serieId: ScatterPlotRawSerie<RawDatum>['id']  }>
-    nodeSize:
-        | number
-        | ScatterPlotNodeDynamicSizeSpec
-        | PropertyAccessor<Omit<ScatterPlotNodeData<RawDatum>, 'size' | 'color'>, number>
-    renderWrapper?: boolean
-    isInteractive: boolean
-    useMesh: boolean
-    debugMesh: boolean
-    onMouseEnter: ScatterPlotMouseHandler<RawDatum>
-    onMouseMove: ScatterPlotMouseHandler<RawDatum>
-    onMouseLeave: ScatterPlotMouseHandler<RawDatum>
-    onClick: ScatterPlotMouseHandler<RawDatum>
-    tooltip: ScatterPlotTooltip<RawDatum>
-    annotations: AnnotationMatcher<ScatterPlotNodeData<RawDatum>>[]
-    legends: LegendProps[]
-    role: string
-    ariaLabel: AriaAttributes['aria-label']
-    ariaLabelledBy: AriaAttributes['aria-labelledby']
-    ariaDescribedBy: AriaAttributes['aria-describedby']
-}
-
-export type ScatterPlotSvgProps<RawDatum extends ScatterPlotDatum> = Partial<
-    ScatterPlotCommonProps<RawDatum>
-> &
-    ScatterPlotDataProps<RawDatum> &
-    Dimensions &
-    MotionProps & {
-        blendMode?: CssMixBlendMode
-        layers?: (ScatterPlotLayerId | ScatterPlotCustomSvgLayer<RawDatum>)[]
-        nodeComponent?: ScatterPlotNode<RawDatum>
-        markers?: CartesianMarkerProps<RawDatum['x'] | RawDatum['y']>[]
-    }
-
-export type ScatterPlotCanvasProps<RawDatum extends ScatterPlotDatum> = Partial<
-    ScatterPlotCommonProps<RawDatum>
-> &
-    ScatterPlotDataProps<RawDatum> &
-    Dimensions & {
-        layers?: (ScatterPlotLayerId | ScatterPlotCustomCanvasLayer<RawDatum>)[]
-        pixelRatio?: number
-        renderNode?: (ctx: CanvasRenderingContext2D, node: ScatterPlotNodeData<RawDatum>) => void
-    } */
-
-//IntrinsicAttributes & Omit<ScatterPlotSvgProps<ScatterPlotDatum>, "width" | "height">
-
-//ScatterPlotRawSerie<ScatterPlotDatum>[]
-//OrdinalColorScaleConfig
-//////////////////////////////////////////////////////////////////////////////////////////////////////////
 interface DataJsonTypes {
   affected_natural: string;
   affected_percentage: string;
@@ -247,19 +33,17 @@ interface DataListTypes {
   data: Array<DataTypes>;
 }
 
-interface ColorObject {
-  Alto: string;
-  Medio: string;
-  Bajo: string;
-}
-///////////////////////////////////////////////colores y tamaños////////////////////////////////
 let dataList: Array<DataListTypes> = [];
-let colorsObj; //: ColorObject
+let colorsObj;
 let id: string;
 let sizes;
-let dataBioma: string;
-let getColor:(serieId: string ) => string;
-let getSize: (serieId: string | number, val: string, biome: string) => number;
+let dataBiome: string;
+let getColor: (serieId: string) => string;
+let getSize: (
+  serieId: string | number,
+  xValue: string,
+  biome: string
+) => number;
 
 export const DotsScatterPlot: React.FC<ScatterProps> = ({
   activeBiome,
@@ -274,14 +58,14 @@ export const DotsScatterPlot: React.FC<ScatterProps> = ({
       parseFloat(axis.fc) > 6.5 &&
       parseFloat(axis.affected_percentage) > 12
     ) {
-      id = "Alto";
+      id = "High";
     } else if (
       parseFloat(axis.fc) < 6.5 &&
       parseFloat(axis.affected_percentage) < 12
     ) {
-      id = "Bajo";
+      id = "Low";
     } else {
-      id = "Medio";
+      id = "Medium";
     }
     return {
       id: id,
@@ -296,25 +80,25 @@ export const DotsScatterPlot: React.FC<ScatterProps> = ({
     };
   });
   getColor = (serieId) => {
-    if (dataBioma === activeBiome && serieId === "Alto") {
+    if (dataBiome === activeBiome && serieId === "High") {
       return colors[4];
     }
-    if (dataBioma === activeBiome && serieId === "Medio") {
+    if (dataBiome === activeBiome && serieId === "Medium") {
       return colors[5];
     }
-    if (dataBioma === activeBiome && serieId === "Bajo") {
+    if (dataBiome === activeBiome && serieId === "Low") {
       return colors[6];
     }
     colorsObj = {
-      Alto: colors[2],
-      Medio: colors[0],
-      Bajo: colors[1],
+      High: colors[2],
+      Medium: colors[0],
+      Low: colors[1],
     };
     return colorsObj[serieId] ?? colors[3];
   };
-  getSize = (serieId, val, biome) => {
-    dataBioma = biome;
-    let x = parseFloat(val);
+  getSize = (serieId, xValue, biome) => {
+    dataBiome = biome;
+    let x = parseFloat(xValue);
     if (biome === activeBiome) {
       if (x >= 80) {
         return x - 50;
@@ -322,9 +106,9 @@ export const DotsScatterPlot: React.FC<ScatterProps> = ({
       return x + 20;
     }
     sizes = {
-      Alto: x >= 80 ? 30 : x + 4,
-      Medio: x + 4,
-      Bajo: x + 4,
+      High: x >= 80 ? 30 : x + 4,
+      Medium: x + 4,
+      Low: x + 4,
     };
     return sizes[serieId] ?? 30;
   };
@@ -366,15 +150,13 @@ export const DotsScatterPlot: React.FC<ScatterProps> = ({
       enableGridX={true}
       enableGridY={true}
       nodeSize={(obj) => {
-        console.log(obj);
-
         return getSize(
           obj.serieId,
           String(obj.formattedX),
           String(obj.data.biome)
         );
       }}
-      tooltip={({ node }: ScatterPlotTooltipProps<ScatterPlotDatum>) => {
+      tooltip={({ node }) => {
         return (
           <div
             style={{
