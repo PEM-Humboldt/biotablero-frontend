6a226d2156b5928b9be7038005c41be3f723083a
Alfio Efren Barragan Castillo <alfiobarragan@iMacIAVH5721DICO.local>
2025-03-24 15:56:31 -0500

    Ajustes-home

--- src/app/layout/Header.tsx
+++ src/app/layout/Header.tsx
@@ -4,45 +4,45 @@ import Menu from "app/layout/header/Menu";
 import Title from "app/layout/header/Title";
 
 interface Names {
-  parent?: string;
-  child?: string;
+    parent?: string;
+    child?: string;
 }
 
 interface HeaderProps {
-  activeModule: string;
-  headerNames: Names;
-  uim: React.ReactNode;
+    activeModule: string;
+    headerNames: Names;
+    uim: React.ReactNode;
 }
 
-const Header: React.FunctionComponent<HeaderProps> = ({
-  activeModule,
-  headerNames: { parent, child },
-  uim,
-}) => (
-  <header className="cabezote">
-    <div className="cabezoteLeft">
-      <nav>
-        <Menu />
-      </nav>
-      <Title title="BioTablero" subTitle={activeModule} />
-    </div>
-    {/* TODO: Sending active user information: image, userName, ...
+const Header: React.FunctionComponent<HeaderProps> = ({ activeModule, headerNames: { parent, child }, uim }) => (
+    <header className="cabezote">
+        <div className="cabezoteLeft">
+            <Title title="BioTablero" subTitle={activeModule} />
+        </div>
+        {/* TODO: Sending active user information: image, userName, ...
         to be upload when user is active */}
-    <div className="header_info">
-      <div className="cabezoteRight">
-        {activeModule && !parent && !child && <h2>{`${activeModule}`}</h2>}
-        {parent && child && (
-          <h1>
-            <b>{`${child}`}</b>
-            <br />
-            {parent}
-          </h1>
-        )}
-        <div className={`${activeModule.replace(/ /g, "")}`} />
-      </div>
-      {uim}
-    </div>
-  </header>
+
+        <div className="cabezoteRight">
+            <nav>
+                <Menu />
+            </nav>
+
+            <div className="header_info">
+                <div className="cabezoteRight">
+                    {activeModule && !parent && !child && <h2>{`${activeModule}`}</h2>}
+                    {parent && child && (
+                        <h1>
+                            <b>{`${child}`}</b>
+                            <br />
+                            {parent}
+                        </h1>
+                    )}
+                    <div className={`${activeModule.replace(/ /g, "")}`} />
+                </div>
+                {uim}
+            </div>
+        </div>
+    </header>
 );
 
 export default Header;
---
+++ src/pages/home/Cenefa.jsx
@@ -0,0 +1,9 @@
+const Cenefa = ({ title, alignClass }) => {
+    return (
+        <div className="Cenefa">
+            <h1 className={` ${alignClass}`}>{title}</h1>
+        </div>
+    );
+};
+
+export default Cenefa;
