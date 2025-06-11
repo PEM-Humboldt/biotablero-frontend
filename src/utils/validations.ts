9daa1ad3fdac62c81d9cac5c36b0dae3ddf0c231
Manuel Gálvez <mgalvez@humboldt.org.co>
2025-06-11 09:42:20 -0500

    Run prettier

--- src/utils/validations.ts
+++ src/utils/validations.ts
@@ -1,7 +1,7 @@
 /**
  * Validate if a variable is undefined or null
  * @param {any} variable variable to be validated
- * 
+ *
  * @returns True if the variable is null or undefined, false otherwise
  */
 export const isUndefinedOrNull = (variable: any) =>
