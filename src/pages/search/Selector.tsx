92b8bf1da39bd7940fc1422b14d98c0684e30496
Denis Armenta <darmenta@humboldt.org.co>
2025-07-31 16:04:47 -0500

    Fix formatting in AREA_ERROR_MESSAGES and whitespace
    Corrected formatting of the 'request-failed' error message in AREA_ERROR_MESSAGES and removed unnecessary whitespace in Selector.tsx for improved code readability.

--- src/pages/search/Selector.tsx
+++ src/pages/search/Selector.tsx
@@ -28,8 +28,7 @@ const Selector: React.FC<Props> = ({ setShowDrawControl }) => {
   const [isLoadingAreaTypes, setIsLoadingAreaTypes] = useState(false);
   const AREA_ERROR_MESSAGES: Record<AreasErrorType, string> = {
     none: "",
-    "request-failed":
-    "Hubo un error en esta funcionalidad, prueba otra alternativa.",
+    "request-failed":"Hubo un error en esta funcionalidad, prueba otra alternativa.",
     "empty-result": "No se encontraron áreas disponibles para consultar.",
   };
 
@@ -45,7 +44,7 @@ const Selector: React.FC<Props> = ({ setShowDrawControl }) => {
 
   useEffect(() => {
     setIsLoadingAreaTypes(true);
-    
+
     isFlagEnabled("drawPolygon").then((value) => setDrawPolygonFlag(value));
     SearchAPI.requestAreaTypes()
       .then((result) => {
