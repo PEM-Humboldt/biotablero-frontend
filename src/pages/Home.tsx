98ec2d91337e9964abc8b0b66323ee1821730378
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2025-07-31 14:39:36 -0500

    Adds types to Home component and translate states

---
+++ src/pages/Home.tsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import { Carrousel } from "pages/home/Carrousel";
+import Tabsmodulos from "pages/home/TabsComponent";
+import "newStyles.css";
+import "headerFooter.css";
+
+type HomeProps = { referrer: string };
+
+export function Home({ referrer }: HomeProps) {
+  const [activeTab, setActiveTab] = useState<number | null>(0);
+  const [showWhatIs, setShowWhatIs] = useState<boolean>(true);
+
+  return (
+    <div>
+      <Carrousel {...{ setActiveTab, setShowWhatIs }} />
+      <Tabsmodulos {...{ activeTab, setActiveTab }} />
+    </div>
+  );
+}
