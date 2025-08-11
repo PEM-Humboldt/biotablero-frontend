cae98e8c045fe55acc2b4208d3058aa8c6f196cc
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2025-08-11 16:08:57 -0500

    Ends the update to the dev environment in Vite

--- src/index.jsx
+++ src/index.jsx
@@ -2,20 +2,26 @@ import ReactDOM from "react-dom";
 import ReactGA from "react-ga4";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { StrictMode } from "react";
 // TODO: Habilitar registerServiceWorker cuando esté habilitado el dominio en HTTPS
 // import registerServiceWorker from './registerServiceWorker';
 
-ReactGA.initialize(import.meta.env.VITE_GA_TRACKING_ID, {
-  gaOptions: {
-    cookieDomain: ".humboldt.org.co",
-    cookieFlags: "SameSite=None; Secure",
-  },
-});
+// Sólo ejecuta google analitycs cuando está en producción
+if (import.meta.env.MODE === "production") {
+  ReactGA.initialize(import.meta.env.VITE_GA_TRACKING_ID, {
+    gaOptions: {
+      cookieDomain: ".humboldt.org.co",
+      cookieFlags: "SameSite=None; Secure",
+    },
+  });
+}
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
   document.getElementById("root")
 );
 // registerServiceWorker();
--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,27 @@ import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), svgr()],
-
   server: {
     port: 3000,
     open: true,
   },
-
   // NOTE: por legacy con CRA mantenemos 'build', vite por default utiliza 'dist'
   build: {
     outDir: "build",
+    sourcemap: true,
+  },
+  // HACK: mientras la migración de componentes, hacemos explícita la declaración
+  // de variables que CRA hacía bajo cuerda para los polifils
+  define: {
+    global: "window",
+    "process.env": {},
+  },
+  optimizeDeps: {
+    include: [],
+    // HACK: Mientras se hace la actualización de dependencias, para evitar
+    // que la consola se inunde con warnings sobre sourcemaps de las librerias
+    esbuildOptions: {
+      sourcemap: false,
+    },
   },
 });
