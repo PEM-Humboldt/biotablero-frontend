c1652f0fa33a8cc2b6f42e9599bca5e9e8173204
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2025-12-13 10:10:19 -0500

    adds caching for the municipalities data

--- src/pages/monitoring/utils/manageLocation.ts
+++ src/pages/monitoring/utils/manageLocation.ts
@@ -2,8 +2,17 @@ import { getLocation } from "pages/monitoring/api/monitoringAPI";
 
 export const COLOMBIAN_DEPARTMENTS = await getLocation();
 
+const municipalitiesCache: {
+  [key: string | number]: { name: string; value: number }[];
+} = {};
+
 export async function getMunicipalitiesByDepartment(
   departmentId: number | string,
 ) {
-  return await getLocation(departmentId);
+  if (departmentId in municipalitiesCache) {
+    return municipalitiesCache[departmentId];
+  }
+
+  municipalitiesCache[departmentId] = await getLocation(departmentId);
+  return municipalitiesCache[departmentId];
 }
