9a1acb63d4309f6f655cf638e1da119f1c4860b4
cgalvis <cgalvis@humboldt.org.co>
2026-03-03 17:50:44 -0500

    refactor: solved eslint issues

--- src/pages/monitoring/outlets/initiativeJoinInvitation/utils/formClientValidations.ts
+++ src/pages/monitoring/outlets/initiativeJoinInvitation/utils/formClientValidations.ts
@@ -1,26 +1,30 @@
-import type { FormClientValidation } from "pages/monitoring/outlets/initiativesAdmin/types/form";
+import type { FormClientValidation } from "pages/monitoring/types/formValidation";
 import type { JoinInitiativeDataForm } from "pages/monitoring/outlets/initiativeJoinInvitation/types/initiativeInvitationData";
 import { uiText } from "pages/monitoring/outlets/initiativeJoinInvitation/layout/uiText";
 
 export const invitationValidations: FormClientValidation<JoinInitiativeDataForm>[] =
   [
     {
-      condition: (f) => Boolean(f.initiativeId) && f.initiativeId > 0,
+      condition: (f: JoinInitiativeDataForm) =>
+        Boolean(f.initiativeId) && f.initiativeId > 0,
       path: "initiativeId",
       message: uiText.form.validation.initiativeIdRequired,
     },
     {
-      condition: (f) => f.guests && f.guests.length > 0,
+      condition: (f: JoinInitiativeDataForm) =>
+        Boolean(f.guests) && f.guests.length > 0,
       path: "guests",
       message: uiText.form.validation.emailsRequired,
     },
     {
-      condition: (f) => {
+      condition: (f: JoinInitiativeDataForm) => {
         if (!f.guests || f.guests.length === 0) {
           return true;
         }
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        return f.guests.every((g) => emailRegex.test(g.email));
+        return f.guests.every((g: { email: string }) =>
+          emailRegex.test(g.email),
+        );
       },
       path: "guests",
       message: uiText.form.validation.badEmailFormat,
