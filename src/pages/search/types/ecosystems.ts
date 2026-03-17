d6f44066d12a2f930cc70892a3355d65828f68da
Manuel Galvez <mgalvez@humboldt.com>
2026-03-16 20:28:29 -0500

    Add types for Strategic Ecosystems

--- src/pages/search/types/ecosystems.ts
+++ src/pages/search/types/ecosystems.ts
@@ -15,3 +15,21 @@ export interface seDetails {
   national_percentage: number;
   total_area: string;
 }
+
+export type SEKey = "paramo" | "tropicalDryForest" | "wetland";
+
+export const SE_LABELS: Record<SEKey, string> = {
+  paramo: "Páramo",
+  tropicalDryForest: "Bosque Seco Tropical",
+  wetland: "Humedal",
+};
+
+export interface SEData {
+  type: SEKey;
+  area: number;
+  values: Record<string, number>;
+}
+
+export interface SEDataExtended extends SEData {
+  percentage: number;
+}
