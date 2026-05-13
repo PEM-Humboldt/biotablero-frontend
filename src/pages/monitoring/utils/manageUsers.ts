f0a4ca972b2d47dc94355bbc4dbd003bd2c59287
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2026-05-13 13:09:13 -0500

    Fix all user levels unintended fetchs

--- src/pages/monitoring/utils/manageUsers.ts
+++ src/pages/monitoring/utils/manageUsers.ts
@@ -3,16 +3,32 @@ import type { UserItem, UserLevel } from "pages/monitoring/types/catalog";
 import type { ODataUserInfo } from "pages/monitoring/types/odataResponse";
 import { isMonitoringAPIError } from "pages/monitoring/api/types/guards";
 
-export const USER_LEVELS: UserLevel[] = await (async () => {
-  const userLevels = await getUserLevels();
-  if (isMonitoringAPIError(userLevels)) {
-    return [];
-  }
-  return userLevels as UserLevel[];
-})();
+function fetchUserLevels() {
+  let userLevels: UserLevel[] = [];
 
-export const NEW_ADMIN_CREDENTIALS = USER_LEVELS[0];
+  return async () => {
+    if (userLevels.length > 0) {
+      return userLevels;
+    }
+    const res = await getUserLevels();
+    if (isMonitoringAPIError(res)) {
+      return [];
+    }
 
+    userLevels = res;
+
+    return userLevels;
+  };
+}
+
+export const userLevels = fetchUserLevels();
+
+/*
+ * It takes the sources of info and makes a normalized user object
+ *
+ * @param usersRaw - The OData response containig basic user info
+ * @returns An array containigg all the users info
+ */
 export function normalizeUsersFromOData(
   usersRaw: ODataUserInfo,
 ): Partial<UserItem>[] {
