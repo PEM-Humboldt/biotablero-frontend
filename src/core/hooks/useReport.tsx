a263991bd3f32e267060ca5d34230a9028dbc61c
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2026-05-22 16:49:33 -0500

    Adds cleaning heap methods

--- src/core/hooks/useReport.tsx
+++ src/core/hooks/useReport.tsx
@@ -1,4 +1,3 @@
-import { type LatLngBoundsLiteral } from "leaflet";
 import {
   createContext,
   type ReactNode,
@@ -7,7 +6,8 @@ import {
   useState,
 } from "react";
 import { useUserCTX } from "@hooks/UserContext";
-import { domToPng } from "modern-screenshot";
+import { domToBlob, type Options as ScrenshotOptions } from "modern-screenshot";
+import workerUrl from "modern-screenshot/worker?url";
 
 type MapDTO = { title: string; blobUrl: string };
 type GraphDTO = { state: string; blobUrl: string; maps: MapDTO[] };
@@ -18,25 +18,30 @@ type SectionDTO = {
   graphs: GraphDTO[];
 };
 
-type SectionInfo<T extends Record<string, string>> = {
+type SectionInfo = {
   title: string;
   description: string;
   includeMap: boolean;
-  aditionalInfo?: T;
+  aditionalInfo?: Record<string, string>;
 };
 
-type SerializeError = { type: string; action: string };
-
 type ReportContextType = {
-  addSection: <T extends Record<string, string>>(
-    sectionInfo: SectionInfo<T>,
+  addSection: (
+    sectionInfo: SectionInfo,
     graphDOMId: string,
+    graphStateId: string | null,
     map: { mapDOMId: string } | null,
-  ) => Promise<void | string>;
+  ) => Promise<void>;
   documentSections: SectionDTO[];
+  removeSection: (sectionTitle: string) => void;
+  removeGraphFromSection: (sectionTitle: string, graphStateId: string) => void;
+  removeMapFromSection: (
+    sectionTitle: string,
+    graphStateId: string | null,
+    mapIndex: number,
+  ) => void;
   isBusy: boolean;
-  // getPreview:
-  // download:
+  errors: string[];
 };
 
 const ReportContext = createContext<ReportContextType | null>(null);
@@ -45,93 +50,232 @@ export function ReportCTX({ children }: { children: ReactNode }) {
   const { user } = useUserCTX();
   const [isBusy, setIsBusy] = useState(false);
   const [documentSections, setDocumentSections] = useState<SectionDTO[]>([]);
+  const [errors, setErrors] = useState<string[]>([]);
+
   const documentMeta = useRef(
     user ? { creator: user?.name, creatorEmail: user.email } : null,
   );
 
-  const addSection = async <T extends Record<string, string>>(
-    sectionInfo: SectionInfo<T>,
+  const addSection = async (
+    sectionInfo: SectionInfo,
     graphDOMId: string,
+    graphStateId: string | null,
     map: { mapDOMId: string } | null,
   ) => {
     if (!user) {
-      return "Debes ser un usuario registrado para crear reportes";
+      return;
     }
 
     setIsBusy(true);
+    setErrors([]);
 
-    const sectionInfoObject: SectionDTO = {
-      title: sectionInfo.title,
-      description: sectionInfo.description,
-      link: window.location.href,
-      graphs: [],
-      ...sectionInfo.aditionalInfo,
-    };
-
-    const graphElement = document.getElementById(graphDOMId);
-    let mapDTO: MapDTO | null = null;
-    let mapElement: HTMLElement | null = null;
-    let mapTitle = null;
-
-    if (map) {
-      mapElement = document.getElementById(map.mapDOMId);
-
-      if (!mapElement) {
-        setIsBusy(false);
-        console.error("Cannot get the map from the DOM");
-        return "No fue posible obtener el mapa.";
-      }
-
-      mapTitle = mapElement
-        ? mapElement.getElementsByClassName("title")[0].textContent
-        : "";
-
-      mapDTO = {
-        title: mapTitle,
-        blobUrl: await domToPng(mapElement),
+    try {
+      const sectionInfoObject: SectionDTO = {
+        title: sectionInfo.title,
+        description: sectionInfo.description,
+        link: window.location.href,
+        graphs: [],
+        ...sectionInfo.aditionalInfo,
       };
-    }
 
-    if (!graphElement) {
-      setIsBusy(false);
-      console.error("Cannot find the graph in the DOM");
-      return "No fue posible obtener la gráfica.";
-    }
+      const graphElement = document.getElementById(graphDOMId);
+      let mapDTO: MapDTO | null = null;
+      let mapElement: HTMLElement | null = null;
+      let mapTitle = null;
 
-    const graphBLOBUrl = await domToPng(graphElement);
+      const screenshotOptions: ScrenshotOptions = {
+        scale: 2,
+        workerUrl,
+        timeout: 10000,
+      };
 
-    const graphDTO: GraphDTO = {
-      state: "",
-      blobUrl: graphBLOBUrl,
-      maps: mapDTO ? [mapDTO] : [],
-    };
+      if (map) {
+        mapElement = document.getElementById(map.mapDOMId);
 
-    sectionInfoObject.graphs.push(graphDTO);
+        if (!mapElement) {
+          console.error("Cannot get the map from the DOM");
+          return;
+        }
 
-    const preview = window.open();
+        mapTitle =
+          mapElement.getElementsByClassName("title")[0]?.textContent || "";
 
-    if (preview) {
-      if (mapDTO) {
-        const map = preview.document.createElement("img");
-        map.src = mapDTO.blobUrl;
-        map.alt = "Preview";
-        map.style.maxWidth = "100%";
-        preview.document.body.appendChild(map);
+        const mapBlob = await domToBlob(mapElement, screenshotOptions);
+
+        mapDTO = {
+          title: mapTitle,
+          blobUrl: URL.createObjectURL(mapBlob),
+        };
       }
 
-      const graph = preview.document.createElement("img");
-      graph.src = graphDTO.blobUrl;
-      graph.alt = "Preview";
-      graph.style.maxWidth = "100%";
-      preview.document.body.appendChild(graph);
+      if (!graphElement) {
+        console.error("Cannot find the graph in the DOM");
+        return;
+      }
+
+      const graphBlob = await domToBlob(graphElement, screenshotOptions);
+
+      const graphDTO: GraphDTO = {
+        state: "",
+        blobUrl: URL.createObjectURL(graphBlob),
+        maps: mapDTO ? [mapDTO] : [],
+      };
+
+      sectionInfoObject.graphs.push(graphDTO);
+
+      const preview = window.open();
+      if (preview) {
+        if (mapDTO) {
+          const mapImg = preview.document.createElement("img");
+          mapImg.src = mapDTO.blobUrl;
+          mapImg.alt = "Preview mapa";
+          preview.document.body.appendChild(mapImg);
+        }
+
+        const graphImg = preview.document.createElement("img");
+        graphImg.src = graphDTO.blobUrl;
+        graphImg.alt = "Preview gráfica";
+        preview.document.body.appendChild(graphImg);
+      }
+
+      setDocumentSections((oldSections) => [...oldSections, sectionInfoObject]);
+    } catch (error) {
+      console.error("Error while serializing DOM elements:", error);
+    } finally {
+      setIsBusy(false);
+    }
+  };
+
+  const removeSection = (sectionTitle: string) => {
+    const selectedSection = documentSections.find(
+      (s) => s.title === sectionTitle,
+    );
+    if (!selectedSection) {
+      return;
     }
 
-    setIsBusy(false);
-    setDocumentSections((oldSections) => [...oldSections, sectionInfoObject]);
+    selectedSection.graphs.forEach((graph) => {
+      URL.revokeObjectURL(graph.blobUrl);
+
+      graph.maps.forEach((map) => {
+        URL.revokeObjectURL(map.blobUrl);
+      });
+    });
+
+    setDocumentSections((oldSections) =>
+      oldSections.filter((s) => s.title !== sectionTitle),
+    );
+  };
+
+  const removeGraphFromSection = (
+    sectionTitle: string,
+    graphStateId: string,
+  ) => {
+    const sectionIdx = documentSections.findIndex(
+      (s) => s.title === sectionTitle,
+    );
+    if (sectionIdx === -1) {
+      return;
+    }
+
+    const section = documentSections[sectionIdx];
+    if (section.graphs.length <= 1) {
+      return;
+    }
+
+    const graphIdx = section.graphs.findIndex((g) => g.state === graphStateId);
+    if (graphIdx === -1) {
+      return;
+    }
+
+    const graphToRemove = section.graphs[graphIdx];
+    if (graphToRemove.blobUrl && graphToRemove.blobUrl.startsWith("blob:")) {
+      URL.revokeObjectURL(graphToRemove.blobUrl);
+    }
+    graphToRemove.maps.forEach((m) => {
+      if (m.blobUrl && m.blobUrl.startsWith("blob:")) {
+        URL.revokeObjectURL(m.blobUrl);
+      }
+    });
+
+    setDocumentSections((oldSections) => {
+      const newSections = [...oldSections];
+
+      newSections[sectionIdx] = {
+        ...newSections[sectionIdx],
+        graphs: newSections[sectionIdx].graphs.filter(
+          (g) => g.state !== graphStateId,
+        ),
+      };
+
+      return newSections;
+    });
+  };
+
+  const removeMapFromSection = (
+    sectionTitle: string,
+    graphStateId: string | null,
+    mapIndex: number,
+  ) => {
+    const sectionIdx = documentSections.findIndex(
+      (s) => s.title === sectionTitle,
+    );
+    if (sectionIdx === -1) {
+      return;
+    }
+    const section = documentSections[sectionIdx];
+    const graphIdx = graphStateId
+      ? section.graphs.findIndex((g) => g.state === graphStateId)
+      : 0;
+
+    if (
+      section.graphs[graphIdx].maps.length === 1 ||
+      graphIdx === -1 ||
+      !section.graphs[graphIdx]
+    ) {
+      return;
+    }
+
+    const graph = section.graphs[graphIdx];
+    const mapToRemove = graph.maps[mapIndex];
+    if (!mapToRemove) {
+      return;
+    }
+
+    if (mapToRemove.blobUrl && mapToRemove.blobUrl.startsWith("blob:")) {
+      URL.revokeObjectURL(mapToRemove.blobUrl);
+    }
+
+    setDocumentSections((oldSections) => {
+      const newSections = [...oldSections];
+      const newGraphs = [...newSections[sectionIdx].graphs];
+      const newMaps = [...newGraphs[graphIdx].maps];
+      newMaps.splice(mapIndex, 1);
+      newGraphs[graphIdx] = {
+        ...newGraphs[graphIdx],
+        maps: newMaps,
+      };
+      newSections[sectionIdx] = {
+        ...newSections[sectionIdx],
+        graphs: newGraphs,
+      };
+
+      return newSections;
+    });
   };
 
   return (
-    <ReportContext.Provider value={{ addSection, documentSections, isBusy }}>
+    <ReportContext.Provider
+      value={{
+        addSection,
+        documentSections,
+        isBusy,
+        errors,
+        removeSection,
+        removeGraphFromSection,
+        removeMapFromSection,
+      }}
+    >
       {children}
     </ReportContext.Provider>
   );
