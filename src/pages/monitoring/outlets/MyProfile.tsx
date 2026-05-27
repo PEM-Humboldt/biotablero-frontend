c7e85bcf5e5f0cb336e511552b77e72e84757cc7
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2026-05-27 16:41:13 -0500

    Sets the doc structure

--- src/pages/monitoring/outlets/MyProfile.tsx
+++ src/pages/monitoring/outlets/MyProfile.tsx
@@ -3,13 +3,49 @@ import { InitiativeUpdater } from "pages/monitoring/outlets/myProfile/Initiative
 
 export function MyProfile() {
   return (
-    <main className="page-main">
+    <main className="page-main [&>section]:w-full">
       <header>
-        <h3>Tablero de iniciativas</h3>
+        <h3>Mi Perfíl</h3>
       </header>
 
-      <JoinRequests />
-      <InitiativeUpdater />
+      <section className="bg-accent">
+        <h3>Información de mi cuenta</h3>
+        <div>usuario</div>
+        <div>
+          estadisticas
+          <div>Tarjeta de estadistica</div>
+        </div>
+      </section>
+
+      <section>
+        <h3 className="sr-only">Administración de iniciativas</h3>
+        <JoinRequests />
+        <InitiativeUpdater />
+      </section>
+
+      <section aria-labelledby="heading-leader">
+        <h3 id="heading-leader" aria-label="Iniciativas que lidero">
+          lider
+        </h3>
+        <div>Tarjeta de iniciativa</div>
+      </section>
+
+      <section aria-labelledby="heading-collaborator">
+        <h3
+          id="heading-collaborator"
+          aria-label="Iniciativas en las que colaboro"
+        >
+          Collaborador
+        </h3>
+        <div>Tarjeta de iniciativa</div>
+      </section>
+
+      <section aria-labelledby="heading-observer">
+        <h3 id="heading-observer" aria-label="Iniciativas que observo">
+          Collaborador
+        </h3>
+        <div>Tarjeta de observador</div>
+      </section>
     </main>
   );
 }
