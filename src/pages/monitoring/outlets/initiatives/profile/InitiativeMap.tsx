02a8fd801eced0f27c8ed8585edfbe2d62317539
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2026-07-07 12:24:58 -0500

    sets up the initiative map

--- src/pages/monitoring/outlets/initiatives/profile/InitiativeMap.tsx
+++ src/pages/monitoring/outlets/initiatives/profile/InitiativeMap.tsx
@@ -1,20 +1,94 @@
 import { renderToString } from "react-dom/server";
-import { MapContainer, Marker, Tooltip, TileLayer } from "react-leaflet";
+import {
+  MapContainer,
+  Marker,
+  Tooltip,
+  TileLayer,
+  GeoJSON,
+} from "react-leaflet";
+import type { Geometry } from "geojson";
 import L from "leaflet";
 
 import { useInitiativeCTX } from "pages/monitoring/hooks/useInitiativeCTX";
 import { MAP_TILES } from "pages/monitoring/outlets/initiativesMap/layout/layers";
 import { InitiativeIcon } from "pages/monitoring/outlets/initiativesMap/mapFinder/InitiativeIcon";
+import { useEffect, useState } from "react";
+import { getInitiativePolygon } from "pages/monitoring/api/services/initiatives";
+import { isMonitoringAPIError } from "pages/monitoring/api/types/guards";
+import {
+  INITIATIVE_MAP_DEFAULT_ZOOM,
+  INITIATIVES_MAP_STATS_GRAPH_COLORS_GRAD,
+} from "@config/monitoring";
+import { ErrorsList } from "@ui/LabelingWithErrors";
+import { LoadingDiv } from "@ui/LoadingDiv";
 
 export function InitiativeMap() {
   const { initiativeInfo } = useInitiativeCTX();
+  const [isLoading, setIsLoading] = useState(false);
+  const [errors, setErrors] = useState<string[]>([]);
+  const [initiativePolygon, setInitiativePolygon] = useState<Geometry | null>(
+    null,
+  );
 
-  return !initiativeInfo ? null : (
-    <div className="lg:flex-1 w-full md:w-[400px] lg:w-[500px] md:shrink-0 md:sticky md:top-0 md:h-screen bg-accent z-10">
+  useEffect(() => {
+    if (!initiativeInfo) {
+      return;
+    }
+
+    const fetchPolygon = async () => {
+      setIsLoading(true);
+      setErrors([]);
+
+      const res = await getInitiativePolygon(initiativeInfo.id);
+      setIsLoading(false);
+      if (isMonitoringAPIError(res)) {
+        setInitiativePolygon(null);
+
+        if (res.status !== 404) {
+          setErrors(res.data.map((err) => err.msg));
+        }
+        return;
+      }
+
+      setInitiativePolygon(res);
+    };
+
+    void fetchPolygon();
+  }, [initiativeInfo]);
+
+  if (!initiativeInfo) {
+    return null;
+  }
+
+  let mapBounds: L.LatLngBoundsExpression | undefined = undefined;
+
+  if (initiativePolygon) {
+    try {
+      const geoJsonLayer = L.geoJSON(initiativePolygon);
+      const bounds = geoJsonLayer.getBounds();
+      if (bounds.isValid()) {
+        mapBounds = bounds;
+      }
+    } catch (e) {
+      console.error("Error calculando bounds estáticos:", e);
+    }
+  }
+  return (
+    <div className="lg:flex-1 w-full md:w-[400px] lg:w-[500px] md:shrink-0 md:sticky md:top-0 md:h-screen z-10">
       <div className="relative flex-col w-full h-[300px] md:h-full flex">
+        {isLoading && (
+          <LoadingDiv className="absolute z-10 m-2 p-4 bg-muted/90 w-[150px]" />
+        )}
+
+        <ErrorsList
+          errorItems={errors}
+          className="absolute z-10 m-2 p-4 bg-accent/90 [&_span]:text-accent-foreground! [&_svg]:text-accent-foreground! border border-primary rounded-lg"
+        />
         <MapContainer
-          center={initiativeInfo.coordinate}
-          zoom={10}
+          key={`map-initiative-${initiativeInfo.id}-${initiativePolygon ? "with-bounds" : "with-center"}`}
+          bounds={mapBounds}
+          center={mapBounds ? undefined : initiativeInfo.coordinate}
+          zoom={mapBounds ? undefined : INITIATIVE_MAP_DEFAULT_ZOOM}
           scrollWheelZoom={false}
           dragging={false}
           zoomControl={false}
@@ -38,6 +112,20 @@ export function InitiativeMap() {
               {initiativeInfo.name}
             </Tooltip>
           </Marker>
+
+          {initiativePolygon && (
+            <GeoJSON
+              key={`geojson-layer-${initiativeInfo.id}`}
+              data={initiativePolygon}
+              style={{
+                fillColor: INITIATIVES_MAP_STATS_GRAPH_COLORS_GRAD[1],
+                weight: 2,
+                opacity: 1,
+                color: INITIATIVES_MAP_STATS_GRAPH_COLORS_GRAD[9],
+                fillOpacity: 0.7,
+              }}
+            />
+          )}
 
           <TileLayer
             attribution={MAP_TILES[0].attribution}
