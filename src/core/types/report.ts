f58c8e0812ffbddc4c64be684b2ff3bc9cf025ab
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2026-07-24 11:09:10 -0500

    Gets the context and base document from the poc

---
+++ src/core/types/report.ts
@@ -0,0 +1,21 @@
+export type MapDTO = { title: string; blobUrl: string };
+
+export type GraphDTO = {
+  state: string;
+  blobUrl: string;
+  maps: MapDTO[];
+  info?: Record<string, string>;
+};
+
+export type SectionDTO = {
+  title: string;
+  description: string;
+  link: string;
+  graphs: GraphDTO[];
+};
+
+export type SectionInfo = {
+  title: string;
+  description: string;
+  graphInfo?: Record<string, string>;
+};
