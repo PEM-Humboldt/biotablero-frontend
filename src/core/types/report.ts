aa3fc5af817b1b506ca6b0b99bfdd0dd6966a884
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2026-07-27 10:14:36 -0500

    updates report types

--- src/core/types/report.ts
+++ src/core/types/report.ts
@@ -1,10 +1,13 @@
-export type MapDTO = { title: string; blobUrl: string };
+import type { LocationCompleteInfo } from "pages/monitoring/types/odataResponse";
+
+export type MapDTO = {
+  id: string;
+  mapUrl: string;
+};
 
 export type GraphDTO = {
   state: string;
   blobUrl: string;
-  maps: MapDTO[];
-  info?: Record<string, string>;
 };
 
 export type SectionDTO = {
@@ -14,8 +17,91 @@ export type SectionDTO = {
   graphs: GraphDTO[];
 };
 
-export type SectionInfo = {
+type Graph = {
+  id: string;
+  blobUrl: string;
+  creatorNote?: string;
+};
+
+// Consultas
+export type SearchSection = {
   title: string;
   description: string;
   graphInfo?: Record<string, string>;
+  graphs: Graph[];
+  mapUrl?: string;
 };
+
+type SearchContext = {
+  location: {
+    type: string;
+    name: string;
+    id: number;
+  };
+  customPolygon: boolean;
+  searchUrl: string;
+};
+
+// Indicadores
+type IndicatorTag = {
+  name: string;
+  fullName?: string;
+  url: string;
+};
+
+type IndicatorSection = {
+  title: string;
+  type: string;
+  creationDate: string;
+  lastUpdate: string;
+  version: number;
+  BiologicalGroupTag: IndicatorTag[];
+  EcosystemTag: IndicatorTag[];
+  mapUrl?: string;
+  graphs: Graph[];
+  explanation: string;
+  card: {
+    methodology: string;
+    interpretation: string;
+    considerations: string;
+    authorship: string;
+  };
+};
+
+export type IndicatorContext = {
+  initiativeName: string;
+  initiativeShortName?: string;
+  initiativeLocation: LocationCompleteInfo[];
+  initiativeCreationDate: string;
+  initiativeDescription: string;
+  initiativeStats: {
+    area: number;
+    areaUnit: string;
+    localitiesUnderMonitoring: number;
+    monitoringEvents: number;
+  };
+  initiativeUrl: string;
+  politicalTags: IndicatorTag[];
+  socialTags: IndicatorTag[];
+};
+
+// General
+export type ReportMetadata = {
+  creationDate: string;
+  madeBy: { name: string; username?: string; mail: string };
+};
+
+export type ReportInfo = {
+  metadata: ReportMetadata;
+} & (
+  | {
+      type: "Indicator";
+      context: IndicatorContext;
+      sections: IndicatorSection[];
+    }
+  | {
+      type: "Search";
+      context: SearchContext;
+      sections: SearchSection[];
+    }
+);
