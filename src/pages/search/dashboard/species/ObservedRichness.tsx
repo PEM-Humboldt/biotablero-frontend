8f584f5585bbec61f488d6e61260b1e9cc7388be
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2026-08-27 15:58:40 -0500

    adds observed richness ui

--- src/pages/search/dashboard/species/ObservedRichness.tsx
+++ src/pages/search/dashboard/species/ObservedRichness.tsx
@@ -21,6 +21,18 @@ import {
 import { useSearchStateCTX } from "pages/search/hooks/SearchContext";
 import type { textsObject } from "pages/search/types/texts";
 import { speciesGroupLabels } from "pages/search/dashboard/species/commonDictionaries";
+import { CircleAlert, LayersIcon, LeafIcon, MapPin } from "lucide-react";
+import { ResponsiveBar } from "@nivo/bar";
+import { LOCALE } from "@config/monitoring";
+import { cn } from "@ui/shadCN/lib/utils";
+import { GraphLegend } from "@ui/GraphLegend";
+
+const OBSERVED_RICHNESS_GRAPH_KEYS = ["CR", "EN", "VU"];
+const customColorMap: Record<string, string> = {
+  CR: "#5c150c",
+  EN: "#bc472b",
+  VU: "#d98242",
+};
 
 export function ObservedRichness() {
   const [isLoading, setIsLoading] = useState(0);
@@ -82,7 +94,7 @@ export function ObservedRichness() {
   }, [selectedGroup]);
 
   return (
-    <div className="graphcontainer pt6 overflow-hidden">
+    <div className="graphcontainer pt6">
       <h4>Número de especies</h4>
       <IconTooltip title="Interpretación">
         <InfoIcon
@@ -120,13 +132,13 @@ export function ObservedRichness() {
 
       <ErrorsList errorItems={errors} />
 
-      <div className="w-full h-full aspect-video">
+      <div className="">
         {isLoading ? (
           <div className="errorData">Cargando datos...</div>
         ) : (
           <>
-            <div>{JSON.stringify(renderData.current, null, 2)}</div>
-            <div>{JSON.stringify(renderData.context, null, 2)}</div>
+            <ObservedRichnessTable data={renderData.current} />
+            <ObservedRichnessTable data={renderData.context} isContext={true} />
           </>
         )}
       </div>
@@ -136,10 +148,141 @@ export function ObservedRichness() {
         metoText={texts.observedRichness.meto}
         quoteText={texts.observedRichness.quote}
         downloadData={controller.current.getDownloadData(renderData)}
-        downloadName={`cifrasRiquezaObservada_${areaType?.label}_${areaId?.name}.csv`}
+        downloadName={`cifrasRiquezaObservada_${areaType?.label}_${areaId?.name}_vs_contextoPaís.csv`}
         isInfoOpen={showInfoGraph}
         toggleInfo={() => setShowInfoGraph((prev) => !prev)}
       />
+    </div>
+  );
+}
+
+function ObservedRichnessTable({
+  data,
+  isContext = false,
+}: {
+  data: ObservedRichnessDataType | null;
+  isContext?: boolean;
+}) {
+  const { areaId } = useSearchStateCTX();
+  if (!data) {
+    return null;
+  }
+
+  const totalThreatened = data.threatenedTotal || 1;
+  const chartData = [
+    {
+      CR: (data.barValues.CR / totalThreatened) * 100,
+      EN: (data.barValues.EN / totalThreatened) * 100,
+      VU: (data.barValues.VU / totalThreatened) * 100,
+      rawCR: data.barValues.CR,
+      rawEN: data.barValues.EN,
+      rawVU: data.barValues.VU,
+    },
+  ];
+
+  return (
+    <div className="mb-4 border-b border-grey">
+      <address className="flex gap-1 items-center text-sm text-grey-dark font-normal not-italic uppercase my-2">
+        <MapPin size={16} />
+        {isContext ? "Colombia" : areaId?.name}
+      </address>
+
+      <ul
+        className={cn(
+          "flex flex-wrap gap-2",
+          "*:flex-1 *:first:flex-none *:border *:border-grey *:p-2 *:rounded-lg",
+          "[&_li>span:first-child]:text-[#888] [&_li>span:first-child]:text-sm [&_li>span:first-child]:font-normal [&_li>span:first-child]:flex [&_li>span:first-child]:gap-1 [&_li>span:first-child]:items-center",
+        )}
+      >
+        <li
+          className={cn(
+            "w-full",
+            isContext ? "flex gap-2 justify-between" : "",
+          )}
+          title={"Total especies observadas"}
+        >
+          <span>
+            <LeafIcon size={14} /> Total especies observadas
+          </span>
+          <span
+            className={cn("font-black", isContext ? "text-lg" : "text-4xl")}
+          >
+            {data.total.toLocaleString(LOCALE)}
+          </span>
+        </li>
+        <li title={"Especies endémicas"}>
+          <span>
+            <MapPin size={14} /> Endémicas
+          </span>
+          <span
+            className={cn("font-black", isContext ? "text-base" : "text-xl")}
+          >
+            {data.endemic}
+          </span>
+        </li>
+        <li title={"Amenazadas"}>
+          <span>
+            <CircleAlert size={14} /> Amenazadas
+          </span>
+          <span
+            className={cn("font-black", isContext ? "text-base" : "text-xl")}
+          >
+            {data.threatenedTotal}
+          </span>
+        </li>
+        <li title={"Endémicas amenazadas"}>
+          <span>
+            <CircleAlert size={14} /> Endém. amenz.
+          </span>
+          <span
+            className={cn("font-black", isContext ? "text-base" : "text-xl")}
+          >
+            {data.endemicThreatened}
+          </span>
+        </li>
+        <li title={"Invasoras"}>
+          <span>
+            <LayersIcon size={14} /> Invasoras
+          </span>
+          <span
+            className={cn("font-black", isContext ? "text-base" : "text-xl")}
+          >
+            {data.invasive}
+          </span>
+        </li>
+      </ul>
+
+      <div className="mt-2">
+        <span className="flex gap-2 justify-between text-[#888] text-sm font-normal">
+          <span>Amenazadas por categoría UICN</span>
+          <span className="text-grey-dark font-bold">
+            {data.threatenedTotal.toLocaleString(LOCALE)}
+          </span>
+        </span>
+        <div className="h-6 w-full rounded-lg overflow-hidden">
+          <ResponsiveBar
+            data={chartData}
+            keys={OBSERVED_RICHNESS_GRAPH_KEYS}
+            layout="horizontal"
+            margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
+            padding={0}
+            colors={({ id }) => customColorMap[id as string] ?? "#FF0000"}
+            valueScale={{ type: "linear", min: 0, max: 100 }}
+            enableGridY={false}
+            enableGridX={false}
+            axisLeft={null}
+            axisBottom={null}
+            enableLabel={false}
+            isInteractive={false}
+          />
+        </div>
+        <GraphLegend
+          keys={OBSERVED_RICHNESS_GRAPH_KEYS}
+          customColorMap={customColorMap}
+          renderValues={data.barValues}
+          className="justify-start px-0 pt-1 text-[#888]!"
+        />
+      </div>
     </div>
   );
 }
