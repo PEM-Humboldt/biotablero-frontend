b63fdae77d3d113c06dced78ddf44c13c2b78c21
Camilo Zapata <cazm013@gmail.com>
2026-09-04 06:59:26 -0500

    Delete mapMetricInfoToTexts mapper as a independent function

--- src/pages/search/utils/texts.ts
+++ src/pages/search/utils/texts.ts
@@ -1,29 +1,30 @@
 import SearchAPI from "pages/search/api/searchAPI";
 import { TextsObject } from "pages/search/types/texts";
-import { MetricInfoResponse } from "../types/metrics";
 import { MetricsTypes } from "../types/metrics";
 
-export const mapMetricInfoToTexts = (
-  metricInfoList: Array<MetricInfoResponse> = [],
-): TextsObject => {
-  const value: TextsObject = {
-    info: "",
-    cons: "",
-    meto: "",
-    quote: "",
-    helper: "",
-  };
-
-  metricInfoList.forEach(({ type, description }) => {
-    value[type] = description;
-  });
-
-  return value;
-};
-
+/**
+ * Gets the texts for a given metric.
+ *
+ * @param {MetricsTypes} metric Metric identifier
+ * @returns {Promise<TextsObject>} Promise containing the metric texts
+ * @throws {Error} If the metric information cannot be retrieved
+ */
 export const getMetricTexts = (metric: MetricsTypes): Promise<TextsObject> =>
   SearchAPI.requestMetricsInfo(metric)
-    .then(mapMetricInfoToTexts)
+    .then((res) => {
+      const value: TextsObject = {
+        info: "",
+        cons: "",
+        meto: "",
+        quote: "",
+        helper: "",
+      };
+      res.forEach(({ type, description }) => {
+        value[type] = description;
+      });
+      return value;
+    })
     .catch(() => {
       throw new Error("Error getting data");
     });
+
