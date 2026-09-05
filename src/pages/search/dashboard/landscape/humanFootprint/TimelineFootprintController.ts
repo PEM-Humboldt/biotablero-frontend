cd3e3747d8c01e71a7f941b2034380c5cad9385d
Miguel Mejía <121742455+dothedada@users.noreply.github.com>
2026-09-05 11:34:35 -0500

    Adds the SE data getter

--- src/pages/search/dashboard/landscape/humanFootprint/TimelineFootprintController.ts
+++ src/pages/search/dashboard/landscape/humanFootprint/TimelineFootprintController.ts
@@ -1,72 +1,85 @@
 import SearchAPI from "pages/search/api/searchAPI";
-import { RasterLayer } from "pages/search/types/layers";
-import { CancelTokenSource } from "axios";
-import { TimelineHF } from "pages/search/types/humanFootprint";
+import type { RasterLayer } from "pages/search/types/layers";
+import { type CancelTokenSource } from "axios";
+import type { TimelineHF } from "pages/search/types/humanFootprint";
 import { MetricsUtils } from "pages/search/utils/metrics";
 import LayerAPI from "pages/search/api/layerAPI";
 
 type SEKeys = Record<"paramo" | "dryForest" | "wetland", string>;
-type TimelineMetricKey = "timelineHF";
 
 export class TimelineFootprintController {
   areaType: string = "";
-  areaId: string = "";
+  areaId: number = 0;
   activeRequests: Map<string, CancelTokenSource> = new Map();
 
   constructor() {}
 
-  setArea(areaType: string, areaId: string) {
+  setArea(areaType: string, areaId: number) {
     this.areaType = areaType;
     this.areaId = areaId;
   }
 
-  getTimelineData = async (): Promise<Array<TimelineHF>> => {
-    const { request } = SearchAPI.requestMetricsValues<"timelineHF">(
+  async getTimelineData(): Promise<TimelineHF[]> {
+    const { request, source } = SearchAPI.requestMetricsValues<"timelineHF">(
       "timelineHF",
       Number(this.areaId),
     );
+    this.activeRequests.set("timelineData", source);
 
-    return request;
-  };
+    return request
+      .catch((err) => {
+        console.error("Error original:", err);
+        throw new Error("Error getting data");
+      })
+      .finally(() => {
+        this.activeRequests.delete("timelineData");
+      });
+  }
 
-  /**
-   * Get shape layers in GeoJSON format for timeline human footprint component
-   *
-   * @returns { Promise<ShapeLayer> } object with the parameters of the layer
-   */
-  getLayer = async (
-    selectedKey: TimelineMetricKey = "timelineHF",
-  ): Promise<Array<RasterLayer>> => {
-    const { request, source } = SearchAPI.requestMetricsLayer(
-      "timelineHF",
+  async getSEData(): Promise<Record<string, number>> {
+    const wetlandReq = SearchAPI.requestMetricsValues<"wetland">(
+      "wetland",
       this.areaId,
-      selectedKey,
-      Number(this.areaId),
     );
-    this.activeRequests.set(selectedKey, source);
-    const res = await request;
-    this.activeRequests.delete(selectedKey);
+    const dryForestReq = SearchAPI.requestMetricsValues<"tropicalDryForest">(
+      "tropicalDryForest",
+      this.areaId,
+    );
+    const paramoReq = SearchAPI.requestMetricsValues<"paramo">(
+      "paramo",
+      this.areaId,
+    );
 
-    if (typeof res === "string") {
-      throw new Error("request canceled");
-    }
+    this.activeRequests.set("se_wetland", wetlandReq.source);
+    this.activeRequests.set("se_dryForest", dryForestReq.source);
+    this.activeRequests.set("se_paramo", paramoReq.source);
 
-    const { request: layerRequest, source: layerSource } =
-      LayerAPI.getLayerData(res);
-    this.activeRequests.set(`${selectedKey}-blob`, layerSource);
-    const blob = await layerRequest;
-    this.activeRequests.delete(`${selectedKey}-blob`);
-    const data = await MetricsUtils.blobToBase64(blob);
+    return Promise.all([
+      wetlandReq.request,
+      dryForestReq.request,
+      paramoReq.request,
+    ])
+      .then(([wetlandRes, dryForestRes, paramoRes]) => {
+        const { id: _id1, ...wetlandData } = wetlandRes;
+        const { id: _id2, ...dryForestData } = dryForestRes;
+        const { id: _id3, ...paramoData } = paramoRes;
 
-    return [
-      {
-        id: selectedKey,
-        paneLevel: 1,
-        data,
-        selected: false,
-      },
-    ];
-  };
+        return {
+          ...wetlandData,
+          ...dryForestData,
+          ...paramoData,
+        };
+      })
+      .catch((err) => {
+        console.error("Error fetching SE data:", err);
+        throw new Error("Error getting ecosystem details");
+      })
+      .finally(() => {
+        this.activeRequests.delete("se_wetland");
+        this.activeRequests.delete("se_dryForest");
+        this.activeRequests.delete("se_paramo");
+      });
+  }
 
   /**
    * Get shape layers in GeoJSON format for special ecosystems
@@ -75,14 +88,12 @@ export class TimelineFootprintController {
    *
    * @returns { Promise<ShapeLayer> } object with the parameters of the layer
    */
-  getSELayer = async (
-    selectedKey: keyof SEKeys,
-  ): Promise<Array<RasterLayer>> => {
+  async getSELayer(selectedKey: keyof SEKeys): Promise<Array<RasterLayer>> {
     const { request, source } = SearchAPI.requestMetricsLayer(
       "timelineHF",
-      this.areaId,
+      String(this.areaId),
       selectedKey,
-      Number(this.areaId),
+      this.areaId,
     );
     this.activeRequests.set(selectedKey, source);
     const res = await request;
@@ -107,15 +118,16 @@ export class TimelineFootprintController {
         selected: false,
       },
     ];
-  };
+  }
 
   /**
    * Send the cancel signal to all active requests and remove them from the map
    */
   cancelActiveRequests = () => {
-    this.activeRequests.forEach((value, key) => {
+    this.activeRequests.forEach((value) => {
       value.cancel();
-      this.activeRequests.delete(key);
     });
+
+    this.activeRequests.clear();
   };
 }
