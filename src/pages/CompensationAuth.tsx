4f844204ec18ee120076ae214f5bdbb4c0598768
cgalvis <cgalvis@humboldt.org.co>
2026-09-15 12:25:25 -0500

    feat: updated compensation auth restriction message

--- src/pages/CompensationAuth.tsx
+++ src/pages/CompensationAuth.tsx
@@ -5,7 +5,7 @@ import { useLocation, useNavigate, useOutletContext } from "react-router";
 import { Compensation } from "pages/Compensation";
 import type { Names } from "@appTypes/layout";
 import { useUserCTX } from "@hooks/UserCTX";
-import { Modal } from "@mui/material";
+import { TriangleAlert } from "lucide-react";
 
 // HACK: Este componente de redireccionamiento es temporal, existe mientras
 // se actualiza el módulo de compensaciones a un componente de función
@@ -19,10 +19,6 @@ export function RenderCompensation() {
   const renderCompensation = user?.username === "geb";
 
   useEffect(() => {
-    if (!renderCompensation) {
-      return;
-    }
-
     layoutDispatch({
       type: LayoutUpdated.CHANGE_SECTION,
       sectionData: {
@@ -37,28 +33,28 @@ export function RenderCompensation() {
     layoutDispatch({ type: LayoutUpdated.HEADER_NAMES, newHeader: names });
 
   return <>
-    <Modal
-      aria-labelledby="simple-modal-title"
-      aria-describedby="simple-modal-description"
-      open={!renderCompensation}
-      disableAutoFocus
-    >
-      <div className="generalAlarm">
-        <h2>
-          <b>{user?.username == null ? "Acceso restringido" : "Usuario no autorizado"}</b>
-          <br />
-          {user?.username == null ? "Inicie sesión para acceder a la página." : "No tienes permisos para acceder a la página."}
-        </h2>
-        <button
-          type="button"
-          className="closebtn"
-          title="Cerrar"
-        >
-        </button>
+    {!renderCompensation && <>
+      <div className="bg-grey-light h-full">
+        <section className="border-t-100 border-t-accent">
+            <div className="max-w-[1200px] px-4 py-8 mx-auto grid grid-cols-1 gap-4 md:py-16 md:grid-cols-2 md:gap-8">
+              <article
+                // key={card.title}
+                key={user?.username == null ? "Acceso restringido" : "Usuario no autorizado"}
+                className="bg-background p-8 rounded-xl"
+              >
+                <h3 className="flex gap-4 items-center text-primary">
+                  <TriangleAlert  className="size-8" strokeWidth="1.5"/>
+                  {/* {card.title} */}
+                  {user?.username == null ? "Acceso restringido" : "Usuario no autorizado"}
+                </h3>
+                <p className="m-0!">
+                  {user?.username == null ? "Inicie sesión para acceder a la página." : "No tienes permisos para acceder a la página."}
+                </p>
+              </article>
+            </div>
+        </section>
       </div>
-    </Modal>
-    {user?.username != null && <Compensation
-      setHeaderNames={handleSetHeaderNames} user={user}
-    />}
+    </>}
+    {user?.username != null && <Compensation setHeaderNames={handleSetHeaderNames} user={user} />}
   </>;
 }
